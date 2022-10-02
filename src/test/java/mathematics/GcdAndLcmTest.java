8317860f737d1cdeefec5e01523dad6d29a4bae2
hyoguoo <hyoguoo@gmail.com>
2022-10-03 03:37:37 +0900

    solve: 최대공약수와 최소공배수

---
+++ src/test/java/mathematics/GcdAndLcmTest.java
@@ -0,0 +1,23 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 2609
+ */
+
+package mathematics;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GcdAndLcmTest {
+
+    @Test
+    void getGcd() {
+        Assertions.assertEquals(GcdAndLcm.getGcd(24, 18), 6);
+    }
+
+    @Test
+    void getLcm() {
+        Assertions.assertEquals(GcdAndLcm.getGcdAndLcm(24, 18), 72);
+    }
+}
