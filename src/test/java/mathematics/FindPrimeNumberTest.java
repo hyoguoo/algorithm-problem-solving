0b7ecc9060e93520c3a884704df3a734f87b4986
hyoguoo <hyoguoo@gmail.com>
2022-10-03 02:41:26 +0900

    solve: 소수 찾기

---
+++ src/test/java/mathematics/FindPrimeNumberTest.java
@@ -0,0 +1,32 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 1978
+ */
+
+package mathematics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindPrimeNumberTest {
+
+    @Test
+    void countPrimeNumber() {
+        int[] numberArray = {1, 3, 5, 7};
+        int length = 4;
+        assertEquals(FindPrimeNumber.countPrimeNumber(numberArray, length), 3);
+    }
+
+
+    @Test
+    void isPrime() {
+        assertTrue(FindPrimeNumber.isPrime(19));
+    }
+
+    @Test
+    void isNotPrime() {
+        assertFalse(FindPrimeNumber.isPrime(9));
+    }
+}
