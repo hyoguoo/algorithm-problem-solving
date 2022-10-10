e2190c12126ed538def79a18761e1bb823604155
hyoguoo <hyoguoo@gmail.com>
2022-10-10 15:05:37 +0900

    solve: 소수 찾기

---
+++ src/main/java/Mathematics/SumPrimeNumber.java
@@ -0,0 +1,39 @@
+/*
+ * goormlevel
+ * https://level.goorm.io
+ * Monday Challenge: 1주차 문제 4
+ */
+
+package Mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class SumPrimeNumber {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(bufferedReader.readLine());
+        int[] numberArray = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int result = sumPrimeNumber(numberArray, length);
+        System.out.println(result);
+    }
+
+    public static int sumPrimeNumber(int[] numberArray, int length) {
+        int sum = 0;
+        for (int i = 1; i <= length; i++) {
+            if (isPrime(i)) sum += numberArray[i - 1];
+        }
+        return sum;
+    }
+
+    public static boolean isPrime(int number) {
+        if (number == 1 || number < 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/Mathematics/SumPrimeNumberTest.java
@@ -0,0 +1,37 @@
+/*
+ * goormlevel
+ * https://level.goorm.io
+ * Monday Challenge: 1주차 문제 4
+ */
+
+package Mathematics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumPrimeNumberTest {
+
+    @Test
+    void countPrimeNumber() {
+        int[] numberArray = {1, 3, 5, 7};
+        int length = 4;
+        assertEquals(SumPrimeNumber.sumPrimeNumber(numberArray, length), 8);
+    }
+
+
+    @Test
+    void isPrime() {
+        assertTrue(SumPrimeNumber.isPrime(29));
+    }
+
+    @Test
+    void isNotPrimeCase1() {
+        assertFalse(SumPrimeNumber.isPrime(15));
+    }
+
+    @Test
+    void isNotPrimeCase2() {
+        assertFalse(SumPrimeNumber.isPrime(-3));
+    }
+}
