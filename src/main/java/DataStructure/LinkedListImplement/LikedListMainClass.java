4dbd38d5b6d7f99c170f406f3bbae3bc33bd5f1d
hyoguoo <hyoguoo@gmail.com>
2022-10-10 18:50:00 +0900

    add: Singled Linked List 실행 메인 메서드 작성

---
+++ src/main/java/DataStructure/LinkedListImplement/LikedListMainClass.java
@@ -0,0 +1,18 @@
+package DataStructure.LinkedListImplement;
+
+public class LikedListMainClass {
+    public static void main(String[] args) {
+        SingleLinkedList<Integer> singleLinkedList = new SingleLinkedList<>();
+
+        singleLinkedList.add(5);
+        singleLinkedList.add(9);
+        singleLinkedList.remove();
+        singleLinkedList.add(59);
+        singleLinkedList.add(95);
+        int removedData = singleLinkedList.remove(1);
+
+        for (int i = 0; i < singleLinkedList.getSize(); i++) System.out.println(singleLinkedList.getData(i));
+        System.out.println(singleLinkedList.indexOf(95));
+        System.out.println(removedData);
+    }
+}
