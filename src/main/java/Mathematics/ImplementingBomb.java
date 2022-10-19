0eb833bae88c53d62dac328467425c3698036302
hyoguoo <hyoguoo@gmail.com>
2022-10-20 00:53:44 +0900

    solve: 폭탄 구현하기

---
+++ src/main/java/Mathematics/ImplementingBomb.java
@@ -0,0 +1,29 @@
+package Mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class ImplementingBomb {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int arrayLength = array[0];
+        int bombLength = array[1];
+
+        int count = 0;
+        for (int i = 0; i < bombLength; i++) {
+            int[] numberArray = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int num1 = numberArray[0];
+            int num2 = numberArray[1];
+            int plus = 5;
+            if (num1 <= 1 || num1 >= arrayLength) plus--;
+            if (num2 <= 1 || num2 >= arrayLength) plus--;
+            count += plus;
+        }
+        if (arrayLength == 1) System.out.println(bombLength);
+        else System.out.println(count);
+    }
+}
