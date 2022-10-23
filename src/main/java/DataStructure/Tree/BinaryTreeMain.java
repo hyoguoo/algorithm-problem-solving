7e30e340b7f251062ae9eaa9bcec36af3d966b1a
hyoguoo <hyoguoo@gmail.com>
2022-10-24 02:10:28 +0900

    add: 이진 트리 main 클래스
    - 단순 삽입 및 출력의 테스트 형식 코드

---
+++ src/main/java/DataStructure/Tree/BinaryTreeMain.java
@@ -0,0 +1,26 @@
+package DataStructure.Tree;
+
+import java.util.Arrays;
+
+public class BinaryTreeMain {
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.add(5);
+        tree.add(4);
+        tree.add(10);
+        tree.add(7);
+        tree.add(11);
+        tree.add(13);
+        tree.add(6);
+        tree.add(9);
+        tree.add(8);
+        tree.delete(11);
+
+        tree.preorder(tree.root, 0);
+
+        System.out.println(Arrays.toString(tree.bfs().toArray()));
+    }
+
+}
