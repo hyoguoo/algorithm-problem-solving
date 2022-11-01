91c9620e5bd382a69b1339254ea0cb3625d70836
hyoguoo <hyoguoo@gmail.com>
2022-11-01 20:25:22 +0900

    solve: 카이사르 암호

---
+++ src/test/java/Implement/CaesarCipherTest.java
@@ -0,0 +1,22 @@
+package Implement;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaesarCipherTest {
+
+    @Test
+    void offsetCharCase1() {
+        char result = CaesarCipher.offsetChar('M', 3);
+
+        assertEquals(result, 'J');
+    }
+
+    @Test
+    void offsetCharCase2() {
+        char result = CaesarCipher.offsetChar('A', 3);
+
+        assertEquals(result, 'X');
+    }
+}
