f8d350c9dc948b372297705a0c7e3b24bfde7bfe
hyoguoo <hyoguoo@gmail.com>
2022-11-02 20:25:45 +0900

    solve: 팩토리얼

---
+++ src/main/java/Mathematics/Factorial.java
@@ -0,0 +1,23 @@
+package Mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Factorial {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println(getFactorial(number));
+    }
+
+    public static long getFactorial(int number) {
+        long result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
