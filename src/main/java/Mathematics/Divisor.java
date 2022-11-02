71daa920ececbd5724bbefc40a3cd0f1d4fbcbc2
hyoguoo <hyoguoo@gmail.com>
2022-11-02 20:25:40 +0900

    solve: 약수

---
+++ src/main/java/Mathematics/Divisor.java
@@ -0,0 +1,21 @@
+package Mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Divisor {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        bufferedReader.readLine();
+        int[] numbers = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(numbers);
+        System.out.println(solution(numbers));
+    }
+
+    public static int solution(int[] numbers) {
+        return numbers[0] * numbers[numbers.length - 1];
+    }
+}
