f8f1522bbd00829e4aeaa3170e65a1468c6dcdd8
hyoguoo <hyoguoo@gmail.com>
2022-11-04 13:28:33 +0900

    chore: 생성자 제거

--- src/test/java/Stack/MakeBiggerTest.java
+++ src/test/java/Stack/MakeBiggerTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 import java.util.Stack;
 
 class MakeBiggerTest {
-    MakeBigger makeBigger = new MakeBigger();
 
     @BeforeEach
     void beforeEach() {
@@ -22,39 +21,39 @@ class MakeBiggerTest {
 
     @Test
     void concatenateStackToString() {
-        makeBigger.stack.push(5);
-        makeBigger.stack.push(9);
+        MakeBigger.stack.push(5);
+        MakeBigger.stack.push(9);
         Assertions.assertEquals(MakeBigger.concatenateStackToString(MakeBigger.stack), "59");
     }
 
     @Test
     void popStackNTimes() {
-        makeBigger.stack.push(5);
-        makeBigger.stack.push(9);
-        makeBigger.stack.push(1);
-        makeBigger.stack.push(2);
-        makeBigger.popStackNTimes(2);
-        Assertions.assertEquals(MakeBigger.concatenateStackToString(makeBigger.stack), "59");
+        MakeBigger.stack.push(5);
+        MakeBigger.stack.push(9);
+        MakeBigger.stack.push(1);
+        MakeBigger.stack.push(2);
+        MakeBigger.popStackNTimes(2);
+        Assertions.assertEquals(MakeBigger.concatenateStackToString(MakeBigger.stack), "59");
     }
 
     @Test
     void makeBiggerNumberToStackCase1() {
         int[] numberArray = {1, 2, 3, 1, 2, 3, 4};
-        makeBigger.makeBiggerNumberToStack(numberArray, 3);
-        Assertions.assertEquals(MakeBigger.concatenateStackToString(makeBigger.stack), "3234");
+        MakeBigger.makeBiggerNumberToStack(numberArray, 3);
+        Assertions.assertEquals(MakeBigger.concatenateStackToString(MakeBigger.stack), "3234");
     }
 
     @Test
     void makeBiggerNumberToStackCase2() {
         int[] numberArray = {7, 8, 9, 8, 1, 1, 1, 1, 0, 1};
-        makeBigger.makeBiggerNumberToStack(numberArray, 4);
-        Assertions.assertEquals(MakeBigger.concatenateStackToString(makeBigger.stack), "981111");
+        MakeBigger.makeBiggerNumberToStack(numberArray, 4);
+        Assertions.assertEquals(MakeBigger.concatenateStackToString(MakeBigger.stack), "981111");
     }
 
     @Test
     void getBiggerStack() {
         int[] numberArray = {7, 8, 9, 8, 1, 1, 1, 1, 0, 1};
-        int count = makeBigger.getBiggerStack(numberArray, 4);
+        int count = MakeBigger.getBiggerStack(numberArray, 4);
         Assertions.assertEquals(count, 1);
     }
 
