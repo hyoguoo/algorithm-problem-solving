8a809e4b51a422405dc49b9c0793aedbbf74a979
hyoguoo <hyoguoo@gmail.com>
2022-11-27 21:42:37 +0900

    solve: 문자열 집합

---
+++ src/main/java/String/SetOfStrings.java
@@ -0,0 +1,30 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 1037
+ */
+
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SetOfStrings {
+
+    static Map<String, Integer> mapOfStrings = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[] numbers = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int count = 0;
+        for (int i = 0; i < numbers[0]; i++) mapOfStrings.put(bufferedReader.readLine(), 0);
+        for (int i = 0; i < numbers[1]; i++) {
+            if (mapOfStrings.containsKey(bufferedReader.readLine())) count++;
+        }
+        System.out.println(count);
+    }
+}
