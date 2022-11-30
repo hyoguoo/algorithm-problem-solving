fef94cf7768d9c80ed37a3fb49d2d92c138e319e
hyoguoo <hyoguoo@gmail.com>
2022-11-30 22:38:17 +0900

    solve: 상수

---
+++ src/main/java/Implement/Constant.java
@@ -0,0 +1,23 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 2908
+ */
+
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Constant {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = bufferedReader.readLine().split(" ");
+
+        int num1 = Integer.parseInt(new StringBuffer(str[0]).reverse().toString());
+        int num2 = Integer.parseInt(new StringBuffer(str[1]).reverse().toString());
+        System.out.println(Math.max(num1, num2));
+    }
+}
