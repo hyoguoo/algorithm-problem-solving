5b859dd3109f19cd682da85db5dece48245bb075
hyoguoo <hyoguoo@gmail.com>
2022-12-03 02:38:25 +0900

    solve: 벌집

---
+++ src/main/java/Mathematics/Honeycomb.java
@@ -0,0 +1,34 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 2292
+ */
+
+package Mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Honeycomb {
+
+    private static final int DIFFERENCE = 6;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int target = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println(getNumberOfLayer(target));
+    }
+
+    private static int getNumberOfLayer(int target) {
+        int sum = 1;
+        int depth = 0;
+
+        while (true) {
+            sum += depth * DIFFERENCE;
+            if (sum >= target) return depth + 1;
+            depth++;
+        }
+    }
+}
