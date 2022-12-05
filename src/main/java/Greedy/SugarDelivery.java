ab27860b5b334c38702b00e8e410e2b2b97ddd3e
hyoguoo <hyoguoo@gmail.com>
2022-12-05 19:45:53 +0900

    solve: 설탕 배달

---
+++ src/main/java/Greedy/SugarDelivery.java
@@ -0,0 +1,37 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 2839
+ */
+
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SugarDelivery {
+
+    static final int THREE = 3;
+    static final int FIVE = 5;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int target = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println(countMinimumBag(target));
+    }
+
+    private static int countMinimumBag(int target) {
+        int bag5count = target / FIVE;
+        int bag3count = 0;
+
+        while (true) {
+            int current = bag5count * FIVE + bag3count * THREE;
+            if (current == target) return bag3count + bag5count;
+            else if (current < target) bag3count++;
+            else bag5count--;
+            if (bag5count < 0) return -1;
+        }
+    }
+}
