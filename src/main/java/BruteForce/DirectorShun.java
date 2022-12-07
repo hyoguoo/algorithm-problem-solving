0ba0c88bcf49615862e001c8ab41f7d26268b953
hyoguoo <hyoguoo@gmail.com>
2022-12-07 16:08:16 +0900

    solve: 영화감독 숌

---
+++ src/main/java/BruteForce/DirectorShun.java
@@ -0,0 +1,32 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 1436
+ */
+
+package BruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DirectorShun {
+
+    static final String SIX = "666";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println(getNthMovie(n));
+    }
+
+    private static int getNthMovie(int n) {
+        int number = 1;
+        while (n > 0) {
+            number++;
+            if (String.valueOf(number).contains(SIX)) n--;
+        }
+        return number;
+    }
+}
