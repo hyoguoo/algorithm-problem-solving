c100b1a73a270e1cfb8e4504841d86d4972e3591
hyoguoo <hyoguoo@gmail.com>
2022-12-08 23:10:42 +0900

    add: counting sort 구현

---
+++ src/main/java/DataStructureImplement/Sort/CountingSort.java
@@ -0,0 +1,40 @@
+package DataStructureImplement.Sort;
+
+public class CountingSort {
+
+    public int[] sort(int[] array) {
+        int[] result = new int[array.length];
+        // 효율적인 정렬을 위해 사용한 것이라면 배열의 범위(min~max)를 미리 알고있어야 한다.
+        int max = getMax(array);
+        int min = getMin(array);
+        int size = max - min + 1;
+        int[] count = new int[size];
+
+        for (int number : array) count[number - min]++;
+        for (int i = 1; i < count.length; i++) count[i] += count[i - 1];
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            int value = array[i];
+            int index = --count[value - min];
+            result[index] = value;
+        }
+
+        return result;
+    }
+
+    private int getMax(int[] array) {
+        int max = array[0];
+        for (int el : array) {
+            if (el > max) max = el;
+        }
+        return max;
+    }
+
+    private int getMin(int[] array) {
+        int min = array[0];
+        for (int el : array) {
+            if (el < min) min = el;
+        }
+        return min;
+    }
+}
--- src/main/java/DataStructureImplement/Sort/SortMain.java
+++ src/main/java/DataStructureImplement/Sort/SortMain.java
@@ -4,12 +4,14 @@ public class SortMain {
 
     public static void main(String[] args) {
         int[] array = {5, 4, 10, 7, 11, 13, 6, 9, 8};
+        int[] countingSortArray = {0, 2, 3, 3, 2, 1, 5, 5, -1, 3, 2, 1, 3, 4, 5, 3, -2, 1, 0};
 
         printArray(new SelectionSort().sort(array));
         printArray(new BubbleSort().sort(array));
         printArray(new InsertionSort().sort(array));
         printArray(new MergeSort().sort(array));
         printArray(new QuickSort().sort(array));
+        printArray(new CountingSort().sort(countingSortArray));
     }
 
     public static void printArray(int[] array) {
