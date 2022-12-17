fa9d30d8feffa8bd60f0dac05ddb29361c7cf19e
hyoguoo <hyoguoo@gmail.com>
2022-12-17 16:20:23 +0900

    solve: 임스와 함께하는 미니게임

---
+++ src/main/java/Map/MiniGamesWithIms.java
@@ -0,0 +1,43 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 25757
+ */
+
+package Map;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MiniGamesWithIms {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] gameInfo = bufferedReader.readLine().split(" ");
+        int gameCount = Integer.parseInt(gameInfo[0]);
+        String gameType = gameInfo[1];
+
+        final Map<String, Integer> nameMap = new HashMap<>();
+        int needPeople = getNeedPeople(gameType);
+        int count = 0;
+
+        for (int i = 0; i < gameCount; i++) {
+            String name = bufferedReader.readLine();
+            if (!nameMap.containsKey(name)) {
+                nameMap.put(name, 0);
+                count++;
+            }
+        }
+        System.out.println(count / needPeople);
+    }
+
+    private static int getNeedPeople(String gameType) {
+        if (gameType.equals("Y")) return 1;
+        if (gameType.equals("F")) return 2;
+        if (gameType.equals("O")) return 3;
+        return -1;
+    }
+}
