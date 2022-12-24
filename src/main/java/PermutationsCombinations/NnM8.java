423d02159c52443fb16b7d20f86dbfc2e08fcf7f
hyoguoo <hyoguoo@gmail.com>
2022-12-25 08:03:43 +0900

    solve: N과 M (8)

---
+++ src/main/java/PermutationsCombinations/NnM8.java
@@ -0,0 +1,42 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 15652
+ */
+
+package PermutationsCombinations;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class NnM8 {
+
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static int[] numbers;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        numbers = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(numbers);
+
+        int[] result = new int[input[1]];
+        recursion(input[0], input[1], result, 0, 0);
+        System.out.println(stringBuilder);
+    }
+
+    private static void recursion(int N, int M, int[] result, int depth, int index) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) stringBuilder.append(result[i]).append(" ");
+            stringBuilder.append("\n");
+            return;
+        }
+
+        for (int i = index; i < N; i++) {
+            result[depth] = numbers[i];
+            recursion(N, M, result, depth + 1, i);
+        }
+    }
+}
