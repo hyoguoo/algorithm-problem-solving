ee6be728f389d06c1315133db42ac091a9925f8d
hyoguoo <hyoguoo@gmail.com>
2023-01-07 13:29:12 +0900

    solve: 가장 긴 증가하는 부분 수열

--- README.md
+++ README.md
@@ -48,3 +48,4 @@
 - String
 - KMP
 - Knapsack
+- LIS
---
+++ src/main/java/DynamicProgramming/LongestIncreasingSubsequence1.java
@@ -0,0 +1,45 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 11053 / 11722
+ * Cheat Level: 3
+ * Algorithm: Dynamic Programing / LIS
+ */
+
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        bufferedReader.readLine();
+        int[] numbers = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] dp = solution(numbers);
+
+        System.out.println(getMaxValue(dp));
+    }
+
+    private static int getMaxValue(int[] dp) {
+        int max = 0;
+        for (int value : dp) max = Math.max(max, value);
+        return max;
+    }
+
+    private static int[] solution(int[] numbers) {
+        int[] dp = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (numbers[j] < numbers[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+
+        return dp;
+    }
+}
