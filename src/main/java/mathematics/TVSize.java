1fe66ced602843e75205c82760b48fad80bbe566
hyoguoo <hyoguoo@gmail.com>
2023-05-27 13:31:23 +0900

    solve: TV 크기

---
+++ src/main/java/mathematics/TVSize.java
@@ -0,0 +1,28 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 1297
+ * Cheat Level: 0
+ * Algorithm: Mathemetics
+ */
+
+package mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class TVSize {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[] info = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        solution(info[0], info[1], info[2]);
+    }
+
+    private static void solution(int D, int H, int W) {
+        double ratio = Math.sqrt(Math.pow(D, 2) / (Math.pow(H, 2) + Math.pow(W, 2)));
+        System.out.println((int) (H * ratio) + " " + (int) (W * ratio));
+    }
+}
