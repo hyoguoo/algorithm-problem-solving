77b38d2e18bcdb60ae34f9f11045a748165ab716
hyoguoo <hyoguoo@gmail.com>
2023-05-28 19:18:15 +0900

    solve: 색종이

---
+++ src/main/java/implementation/Confetti.java
@@ -0,0 +1,48 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 
+ * Cheat Level: 
+ * Algorithm: 
+ */
+
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Confetti {
+
+    final static int CONFETTI_SIZE = 10;
+    final static int PAPER_SIZE = 100;
+    static boolean[][] paper = new boolean[PAPER_SIZE][PAPER_SIZE];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int confettiCount = Integer.parseInt(bufferedReader.readLine());
+        int size = getSize(bufferedReader, confettiCount);
+
+        System.out.println(size);
+    }
+
+    private static int getSize(BufferedReader bufferedReader, int confettiCount) throws IOException {
+        int size = 0;
+
+        for (int i = 0; i < confettiCount; i++) {
+            int[] confettiInfo = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int x = confettiInfo[0];
+            int y = confettiInfo[1];
+            for (int j = x; j < x + CONFETTI_SIZE; j++) {
+                for (int k = y; k < y + CONFETTI_SIZE; k++) {
+                    if (paper[j][k]) continue;
+                    paper[j][k] = true;
+                    size++;
+                }
+            }
+        }
+
+        return size;
+    }
+}
