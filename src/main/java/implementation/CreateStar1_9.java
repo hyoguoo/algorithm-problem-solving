6e1c8b30ea81d4c9f541128cfb9ae9562691eb8c
hyoguoo <hyoguoo@gmail.com>
2023-09-20 10:34:10 +0900

    chore: 파일명 올바르게 변경

--- src/main/java/implementation/CreateStar1_9.java
+++ src/main/java/implementation/CreateStar1_9.java
@@ -12,7 +12,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class CreateStar1_10 {
+public class CreateStar1_9 {
 
     static char star = '*';
     static StringBuilder stringBuilder = new StringBuilder();
