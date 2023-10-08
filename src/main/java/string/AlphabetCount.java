7f3f059afd1766bd987d67ccc71bdb00064aba92
hyoguoo <hyoguoo@gmail.com>
2023-10-08 18:22:19 +0900

    solve: 제곱수의 합

--- src/main/java/string/AlphabetCount.java
+++ src/main/java/string/AlphabetCount.java
@@ -6,21 +6,23 @@
  * Algorithm: String
  */
 
-import java.io.*;
-import java.util.*;
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class AlphabetCount {
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         char[] input = bufferedReader.readLine().toCharArray();
         int[] count = new int[26];
-        
-        for(int i = 0; i < input.length; i++) {
-            count[(int)input[i] - 97]++;
-        }
-        
-        System.out.println(Arrays.toString(count).replaceAll("[\\]\\[\\,]",""));
+
+        for (char c : input) count[c - 97]++;
+
+        System.out.println(String.join(" ", Arrays.stream(count).mapToObj(String::valueOf).toArray(String[]::new)));
     }
 }
