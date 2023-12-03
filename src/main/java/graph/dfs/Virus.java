8440b19acd12944350360083a8a727c64c6f7222
hyoguoo <hyoguoo@gmail.com>
2023-12-03 15:51:07 +0900

    resolve: 바이러스

--- src/main/java/graph/dfs/Virus.java
+++ src/main/java/graph/dfs/Virus.java
@@ -3,7 +3,7 @@
  * https://www.acmicpc.net
  * Problem Number: 2606
  * Cheat Level: 0
- * Algorithm: Graph / DFS
+ * Algorithm: Graph / BFS
  */
 
 package graph.dfs;
@@ -11,42 +11,54 @@ package graph.dfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
 public class Virus {
 
-    static boolean[] visited;
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final int START_VERTEX = 1;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int vertexCount = Integer.parseInt(bufferedReader.readLine());
         int edgeCount = Integer.parseInt(bufferedReader.readLine());
 
-        visited = new boolean[vertexCount + 1];
-        int[][] edges = new int[edgeCount][2];
-        for (int i = 0; i < edgeCount; i++) edges[i] = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Map<Integer, List<Integer>> adjacentVertices = initializeAdjacentVertices(edgeCount);
 
-        dfs(edges, 1);
-        System.out.println(countVisitedVertex() - 1);
+        System.out.println(solution(adjacentVertices, vertexCount));
     }
 
-    private static int countVisitedVertex() {
-        int count = 0;
-        for (boolean b : visited) if (b) count++;
-        return count;
+    private static Map<Integer, List<Integer>> initializeAdjacentVertices(int edgeCount) throws IOException {
+        Map<Integer, List<Integer>> adjacentVertices = new HashMap<>();
+
+        for (int e = 0; e < edgeCount; e++) {
+            int[] edgeInfo = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            adjacentVertices.computeIfAbsent(edgeInfo[0], v -> new ArrayList<>()).add(edgeInfo[1]);
+            adjacentVertices.computeIfAbsent(edgeInfo[1], v -> new ArrayList<>()).add(edgeInfo[0]);
+        }
+
+        return adjacentVertices;
     }
 
-    private static void dfs(int[][] edges, int currentVertex) {
-        visited[currentVertex] = true;
-        for (int[] edge : edges) {
-            if (edge[0] == currentVertex && !visited[edge[1]]) {
-                visited[edge[1]] = true;
-                dfs(edges, edge[1]);
-            }
-            if (edge[1] == currentVertex && !visited[edge[0]]) {
-                visited[edge[0]] = true;
-                dfs(edges, edge[0]);
+    private static int solution(Map<Integer, List<Integer>> adjacentVertices, int vertexCount) {
+        boolean[] visited = new boolean[vertexCount + 1];
+        visited[START_VERTEX] = true;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(START_VERTEX);
+
+        int visitCount = 0;
+
+        while (!queue.isEmpty()) {
+            Integer current = queue.poll();
+            visitCount++;
+
+            for (Integer next : adjacentVertices.getOrDefault(current, Collections.emptyList())) {
+                if (visited[next]) continue;
+                queue.add(next);
+                visited[next] = true;
             }
         }
+
+        return visitCount - 1;
     }
 }
