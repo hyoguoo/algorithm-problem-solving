1410b7d087f2ed4fb373e63cc8044a58bf8b8755
hyoguoo <hyoguoo@gmail.com>
2023-12-06 15:37:36 +0900

    resolve: 듣보잡

--- src/main/java/datastructure/mapset/Anonymous.java
+++ src/main/java/datastructure/mapset/Anonymous.java
@@ -15,23 +15,51 @@ import java.util.*;
 
 public class Anonymous {
 
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int[] numbers = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int x = numbers[0];
-        int y = numbers[1];
+        int[] info = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = info[0];
+        int m = info[1];
 
-        List<String> results = new ArrayList<>();
-        Map<String, Integer> mapOfStrings = new HashMap<>();
-        for (int i = 0; i < x; i++) mapOfStrings.put(bufferedReader.readLine(), 0);
+        solution(n, m);
+    }
 
-        for (int i = 0; i < y; i++) {
-            String str = bufferedReader.readLine();
-            if (mapOfStrings.containsKey(str)) results.add(str);
+    private static void solution(int n, int m) throws IOException {
+        Set<String> wordSet = getWordSet(n);
+        List<String> duplicateList = getDuplicateList(m, wordSet);
+
+        printResult(duplicateList);
+    }
+
+    private static void printResult(List<String> duplicateList) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append(duplicateList.size()).append("\n");
+        for (String word : duplicateList) {
+            stringBuilder.append(word).append("\n");
         }
 
-        Collections.sort(results);
-        System.out.println(results.size());
-        for (String result : results) System.out.println(result);
+        System.out.print(stringBuilder);
+    }
+
+    private static List<String> getDuplicateList(int m, Set<String> wordSet) throws IOException {
+        List<String> duplicateList = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            String str = bufferedReader.readLine();
+            if (wordSet.contains(str)) duplicateList.add(str);
+        }
+
+        Collections.sort(duplicateList);
+        return duplicateList;
+    }
+
+    private static Set<String> getWordSet(int n) throws IOException {
+        Set<String> wordSet = new HashSet<>();
+
+        for (int i = 0; i < n; i++) wordSet.add(bufferedReader.readLine());
+
+        return wordSet;
     }
 }
