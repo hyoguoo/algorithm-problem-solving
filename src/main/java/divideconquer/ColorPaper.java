5b446a98b81eed645b615b9c490677bdc51c8be2
hyoguoo <hyoguoo@gmail.com>
2023-12-18 12:41:11 +0900

    resolve: 색종이 만들기

--- src/main/java/divideconquer/ColorPaper.java
+++ src/main/java/divideconquer/ColorPaper.java
@@ -11,52 +11,53 @@ package divideconquer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class ColorPaper {
 
-    static int[][] paper;
-    static int N;
     static int zeroCount = 0;
     static int oneCount = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bufferedReader.readLine());
-        paper = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            String[] line = bufferedReader.readLine().split(" ");
-            for (int j = 0; j < N; j++) paper[i][j] = Integer.parseInt(line[j]);
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int[][] paper = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            paper[i] = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
 
-        recursion(0, 0, N);
-        System.out.println(zeroCount);
-        System.out.println(oneCount);
+        recursion(paper, n, 0, 0);
+
+        System.out.print(zeroCount + "\n" + oneCount);
     }
 
-    private static void recursion(int x, int y, int length) {
-        if (checkIsCompleted(x, y, length)) return;
+    private static void recursion(int[][] paper, int length, int startX, int startY) {
+        if (isCompletedSquare(paper, length, startX, startY, paper[startX][startY])) return;
 
         int half = length / 2;
+
         if (half == 0) {
-            recursion(x, y, half);
+            recursion(paper, half, startX, startY);
             return;
         }
-        recursion(x, y, half);
-        recursion(x + half, y, half);
-        recursion(x, y + half, half);
-        recursion(x + half, y + half, half);
+
+        recursion(paper, half, startX, startY);
+        recursion(paper, half, startX + half, startY);
+        recursion(paper, half, startX, startY + half);
+        recursion(paper, half, startX + half, startY + half);
     }
 
-    private static boolean checkIsCompleted(int x, int y, int length) {
-        int reference = paper[x][y] == 1 ? 1 : 0;
-        for (int i = x; i < x + length; i++) {
-            for (int j = y; j < y + length; j++) {
-                if (paper[i][j] != reference) return false;
+    private static boolean isCompletedSquare(int[][] paper, int length, int startX, int startY, int reference) {
+        for (int x = startX; x < startX + length; x++) {
+            for (int y = startY; y < startY + length; y++) {
+                if (paper[x][y] != reference) return false;
             }
         }
 
         if (reference == 0) zeroCount++;
         if (reference == 1) oneCount++;
+
         return true;
     }
 }
