728d85d2c2da6273b653777192bc389467a069af
hyoguoo <hyoguoo@gmail.com>
2023-12-24 17:49:04 +0900

    solve: 문서 검색

---
+++ src/main/java/string/SearchDocument.java
@@ -0,0 +1,40 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 1543
+ * Cheat Level: 0
+ * Algorithm: String
+ */
+
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SearchDocument {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String src = bufferedReader.readLine();
+        String target = bufferedReader.readLine();
+
+        System.out.println(solution(src, target));
+    }
+
+    private static int solution(String src, String target) {
+        int count = 0;
+        int index = 0;
+
+        while (index <= src.length() - target.length()) {
+            if (src.startsWith(target, index)) {
+                count++;
+                index += target.length();
+            } else {
+                index++;
+            }
+        }
+
+        return count;
+    }
+}
