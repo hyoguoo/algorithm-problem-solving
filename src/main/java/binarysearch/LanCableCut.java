fd15809d22cdec2f1c82b8f91960cf08e958cbe9
hyoguoo <hyoguoo@gmail.com>
2024-01-01 19:50:51 +0900

    resolve: 랜선 자르기

--- src/main/java/binarysearch/LanCableCut.java
+++ src/main/java/binarysearch/LanCableCut.java
@@ -2,56 +2,49 @@
  * BAEKJOON ONLINE JUDGE
  * https://www.acmicpc.net
  * Problem Number: 1654
- * Cheat Level: 2
+ * Cheat Level: 0
  * Algorithm: Binary Search
  */
 
 package binarysearch;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class LanCableCut {
 
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        int length = scanner.nextInt();
-        int target = scanner.nextInt();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[] info = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        List<Long> cableList = new ArrayList<>();
-        for (int i = 0; i < length; i++) cableList.add(scanner.nextLong());
+        int[] cables = new int[info[0]];
+        int targetCount = info[1];
 
-        Collections.sort(cableList);
-        List<Long> result = binarySearch(cableList, target);
-        System.out.println(Collections.max(result));
-    }
-
-    private static List<Long> binarySearch(List<Long> cableList, int target) {
-        List<Long> resultList = new ArrayList<>();
-        Long max = Collections.max(cableList);
-        Long min = 1L;
-        Long mid;
-
-        while (min <= max) {
-            mid = (min + max) / 2;
-            Integer count = getCount(cableList, mid);
-            if (target <= count) {
-                resultList.add(mid);
-                min = mid + 1;
-            } else {
-                max = mid - 1;
-            }
+        for (int i = 0; i < cables.length; i++) {
+            cables[i] = Integer.parseInt(bufferedReader.readLine());
         }
 
-        return resultList;
+        System.out.println(solution(cables, targetCount));
     }
 
-    private static Integer getCount(List<Long> list, long length) {
-        int count = 0;
-        for (Long number : list) count += number / length;
-        return count;
+    private static long solution(int[] cables, int targetCount) {
+        long left = 1;
+        long right = Arrays.stream(cables).max().orElse(0);
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long count = getCount(cables, mid);
+
+            if (count >= targetCount) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return right;
+    }
+
+    private static long getCount(int[] cables, long mid) {
+        return Arrays.stream(cables).mapToLong(cable -> cable / mid).sum();
     }
 }
