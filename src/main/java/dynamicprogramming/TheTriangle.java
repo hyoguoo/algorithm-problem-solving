08cdc0b356b45635f4b18c8a3557818b6bb5d723
hyoguoo <hyoguoo@gmail.com>
2024-01-08 14:12:49 +0900

    resolve: 정수 삼각형

--- src/main/java/dynamicprogramming/TheTriangle.java
+++ src/main/java/dynamicprogramming/TheTriangle.java
@@ -2,7 +2,7 @@
  * BAEKJOON ONLINE JUDGE
  * https://www.acmicpc.net
  * Problem Number: 1932
- * Cheat Level: 2
+ * Cheat Level: 0
  * Algorithm: Dynamic Programing
  */
 
@@ -15,33 +15,33 @@ import java.util.Arrays;
 
 public class TheTriangle {
 
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int length = Integer.parseInt(bufferedReader.readLine());
 
-        int[] dp = solution(length, bufferedReader);
-        System.out.println(getMaxValue(dp));
+        System.out.print(solution(length));
     }
 
-    private static int getMaxValue(int[] dp) {
-        int max = 0;
-        for (int value : dp) max = Math.max(max, value);
-        return max;
-    }
-
-    private static int[] solution(int length, BufferedReader bufferedReader) throws IOException {
-        int[] dp = new int[length];
-        dp[0] = Integer.parseInt(bufferedReader.readLine());
+    private static int solution(int length) throws IOException {
+        int[] dp = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         for (int i = 2; i <= length; i++) {
             int[] numbers = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
             for (int j = 0; j < i; j++) {
-               if (j == 0) numbers[j] += dp[j];
-               else if (j == i - 1) numbers[j] += dp[j - 1];
-               else numbers[j] += Math.max(dp[j - 1], dp[j]);
+                if (j == 0) numbers[j] += dp[j];
+                else if (j == i - 1) numbers[j] += dp[j - 1];
+                else numbers[j] += Math.max(dp[j - 1], dp[j]);
             }
+
             dp = numbers;
         }
-        return dp;
+
+        return getMaxValue(dp);
+    }
+
+    private static int getMaxValue(int[] dp) {
+        return Arrays.stream(dp).max().orElseThrow();
     }
 }
