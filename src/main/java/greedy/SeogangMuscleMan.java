dcf0c34ad90cf90108cb07f8237f435eb53a7a39
hyoguoo <hyoguoo@gmail.com>
2024-01-23 09:21:14 +0900

    resolve: 서강근육맨

--- src/main/java/greedy/SeogangMuscleMan.java
+++ src/main/java/greedy/SeogangMuscleMan.java
@@ -16,35 +16,30 @@ import java.util.Arrays;
 public class SeogangMuscleMan {
 
     public static void main(String[] args) throws IOException {
-        long[] numbers = init();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        bufferedReader.readLine();
+        long[] numbers = Arrays.stream(bufferedReader.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
         System.out.println(solution(numbers));
     }
 
     private static long solution(long[] numbers) {
-        long maxValue = Long.MIN_VALUE;
-        int length = numbers.length;
+        Arrays.sort(numbers);
 
-        if (length == 1) maxValue = numbers[0];
-        else if (length % 2 == 0) {
-            for (int i = 0; i < length / 2; i++) {
-                maxValue = Math.max(maxValue, numbers[i] + numbers[length - 1 - i]);
+        long maxValue = Long.MIN_VALUE;
+
+        if (numbers.length == 1) {
+            maxValue = numbers[0];
+        } else if (numbers.length % 2 == 0) {
+            for (int i = 0; i < numbers.length / 2; i++) {
+                maxValue = Math.max(maxValue, numbers[i] + numbers[numbers.length - 1 - i]);
             }
         } else {
-            for (int i = 0; i < length / 2; i++) {
-                maxValue = Math.max(maxValue, numbers[i] + numbers[length - 2 - i]);
+            for (int i = 0; i < numbers.length / 2; i++) {
+                maxValue = Math.max(maxValue, numbers[i] + numbers[numbers.length - 2 - i]);
             }
-            maxValue = Math.max(maxValue, numbers[length - 1]);
+            maxValue = Math.max(maxValue, numbers[numbers.length - 1]);
         }
 
         return maxValue;
-    }
-
-    private static long[] init() throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        bufferedReader.readLine();
-        long[] numbers = Arrays.stream(bufferedReader.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
-        Arrays.sort(numbers);
-
-        return numbers;
     }
 }
