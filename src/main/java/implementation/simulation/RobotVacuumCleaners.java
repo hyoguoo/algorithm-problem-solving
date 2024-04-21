3e29575d0cf2f25b0013a5c85daca00e219b52f2
hyoguoo <hyoguoo@gmail.com>
2024-04-21 18:58:31 +0900

    resolve: 로봇 청소기

--- src/main/java/implementation/simulation/RobotVacuumCleaners.java
+++ src/main/java/implementation/simulation/RobotVacuumCleaners.java
@@ -15,69 +15,178 @@ import java.util.Arrays;
 
 public class RobotVacuumCleaners {
 
-    final static int EMPTY = 0;
-    final static int WALL = 1;
-    final static int CLEANED = 2;
-    static Robot robot;
-    static int N, M;
-    static int[][] grid;
-    static int cleanedCount = 0;
+    private static final BufferedReader BUFFERED_READER =
+            new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        init();
-        clean();
-        System.out.println(cleanedCount);
+        int[] mapInfo = Arrays.stream(BUFFERED_READER.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        Robot robot = parseRobotInfo();
+        Grid grid = parseMapInfo(mapInfo[0], mapInfo[1]);
+
+        System.out.print(
+                solution(
+                        robot,
+                        grid
+                )
+        );
     }
 
-    private static void clean() {
-        if (grid[robot.n][robot.m] == EMPTY) {
-            grid[robot.n][robot.m] = CLEANED;
-            cleanedCount++;
+    private static int solution(Robot robot, Grid grid) {
+        int count = 0;
+
+        while (true) {
+            if (grid.isAvailableClean(robot.coordinate)) {
+                robot.clean(grid);
+                count++;
+            }
+
+            if (!grid.checkAround(robot.coordinate)) {
+                if (robot.moveBack(grid)) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            robot.turnLeft();
+            robot.moveFront(grid);
         }
 
-        for (int i = 0; i < 4; i++) {
-            robot.direction = Direction.values()[(robot.direction.ordinal() + 3) % 4];
-            int nextN = robot.n + robot.direction.n;
-            int nextM = robot.m + robot.direction.m;
-            if (grid[nextN][nextM] == EMPTY) {
-                robot.n = nextN;
-                robot.m = nextM;
-                clean();
-                return;
+        return count;
+    }
+
+    private static Grid parseMapInfo(int sizeN, int sizeM) throws IOException {
+        State[][] map = new State[sizeN][sizeM];
+
+        for (int i = 0; i < sizeN; i++) {
+            int[] row = Arrays.stream(BUFFERED_READER.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            for (int j = 0; j < sizeM; j++) {
+                map[i][j] = State.fromValue(row[j]);
             }
         }
 
-        int nextN = robot.n - robot.direction.n;
-        int nextM = robot.m - robot.direction.m;
-        if (grid[nextN][nextM] == WALL) return;
-        robot.n = nextN;
-        robot.m = nextM;
-        clean();
+        return new Grid(map);
     }
 
-    private static void init() throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int[] info = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        N = info[0];
-        M = info[1];
-        grid = new int[N][M];
-        int[] robotInfo = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        robot = new Robot(robotInfo[0], robotInfo[1], Direction.values()[robotInfo[2]]);
-        for (int n = 0; n < N; n++) {
-            grid[n] = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        }
+    private static Robot parseRobotInfo() throws IOException {
+        int[] robotInfo = Arrays.stream(BUFFERED_READER.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int robotN = robotInfo[0];
+        int robotM = robotInfo[1];
+        int robotDirection = robotInfo[2];
+
+        return new Robot(
+                new Coordinate(robotN, robotM),
+                Direction.fromValue(robotDirection)
+        );
     }
 
     enum Direction {
-        NORTH(-1, 0),
-        EAST(0, 1),
-        SOUTH(1, 0),
-        WEST(0, -1);
+        NORTH(0, -1, 0),
+        EAST(1, 0, 1),
+        SOUTH(2, 1, 0),
+        WEST(3, 0, -1);
 
-        final int n;
-        final int m;
+        private final int value;
+        private final int n;
+        private final int m;
 
-        Direction(int n, int m) {
+        Direction(int value, int n, int m) {
+            this.value = value;
+            this.n = n;
+            this.m = m;
+        }
+
+        public static Direction fromValue(int index) {
+            return Arrays.stream(Direction.values())
+                    .filter(direction -> direction.value == index)
+                    .findFirst()
+                    .orElseThrow();
+        }
+
+        public Direction turnLeft() {
+            return Arrays.stream(Direction.values())
+                    .filter(direction -> direction.n == -this.m && direction.m == this.n)
+                    .findFirst()
+                    .orElseThrow();
+        }
+    }
+
+    enum State {
+        CLEANED(2),
+        WALL(1),
+        EMPTY(0);
+
+        private final int value;
+
+        State(int value) {
+            this.value = value;
+        }
+
+        public static State fromValue(int value) {
+            return Arrays.stream(State.values())
+                    .filter(state -> state.value == value)
+                    .findFirst()
+                    .orElseThrow();
+        }
+    }
+
+    static class Grid {
+
+        private final State[][] states;
+
+        public Grid(State[][] states) {
+            this.states = states;
+        }
+
+        public boolean isAvailableClean(Coordinate coordinate) {
+            return this.isInBound(coordinate) &&
+                    this.states[coordinate.n][coordinate.m] == State.EMPTY;
+        }
+
+        public boolean isAvailableMove(Coordinate coordinate) {
+            return this.isInBound(coordinate) &&
+                    this.states[coordinate.n][coordinate.m] != State.WALL;
+        }
+
+        public void clean(Coordinate coordinate) {
+            this.states[coordinate.n][coordinate.m] = State.CLEANED;
+        }
+
+        public boolean checkAround(Coordinate coordinate) {
+            for (Direction direction : Direction.values()) {
+                Coordinate nextCoordinate = new Coordinate(
+                        coordinate.n + direction.n,
+                        coordinate.m + direction.m
+                );
+
+                if (this.isAvailableClean(nextCoordinate)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private boolean isInBound(Coordinate coordinate) {
+            return 0 <= coordinate.n && coordinate.n < this.states.length &&
+                    0 <= coordinate.m && coordinate.m < this.states[0].length;
+        }
+    }
+
+    static class Coordinate {
+
+        private final int n;
+        private final int m;
+
+        public Coordinate(int n, int m) {
             this.n = n;
             this.m = m;
         }
@@ -85,14 +194,48 @@ public class RobotVacuumCleaners {
 
     static class Robot {
 
-        int n;
-        int m;
-        Direction direction;
+        private Coordinate coordinate;
+        private Direction direction;
 
-        public Robot(int n, int m, Direction direction) {
-            this.n = n;
-            this.m = m;
+        public Robot(Coordinate coordinate, Direction direction) {
+            this.coordinate = coordinate;
             this.direction = direction;
+        }
+
+        public boolean moveFront(Grid grid) {
+            Coordinate nextCoordinate = new Coordinate(
+                    this.coordinate.n + this.direction.n,
+                    this.coordinate.m + this.direction.m
+            );
+
+            if (grid.isAvailableClean(nextCoordinate)) {
+                this.coordinate = nextCoordinate;
+                return true;
+            }
+
+            return false;
+        }
+
+        public boolean moveBack(Grid grid) {
+            Coordinate nextCoordinate = new Coordinate(
+                    this.coordinate.n - this.direction.n,
+                    this.coordinate.m - this.direction.m
+            );
+
+            if (grid.isAvailableMove(nextCoordinate)) {
+                this.coordinate = nextCoordinate;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void clean(Grid grid) {
+            grid.clean(this.coordinate);
+        }
+
+        public void turnLeft() {
+            this.direction = this.direction.turnLeft();
         }
     }
 }
