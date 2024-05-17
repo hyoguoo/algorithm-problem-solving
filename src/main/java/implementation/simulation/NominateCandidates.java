902add713c56a8fbaa0337b0d48231d6932418c8
hyoguoo <hyoguoo@gmail.com>
2024-05-17 10:45:39 +0900

    resolve: 후보 추천하기

--- src/main/java/implementation/simulation/NominateCandidates.java
+++ src/main/java/implementation/simulation/NominateCandidates.java
@@ -13,97 +13,98 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Optional;
+import java.util.PriorityQueue;
 
 public class NominateCandidates {
 
-    final static List<Candidate> candidateList = new LinkedList<>();
-    static int CANDIDATE_COUNT;
-    static int[] nominations;
-
     public static void main(String[] args) throws IOException {
-        init();
-        simulation();
-        candidateList.sort(Comparator.comparingInt(o -> o.number));
-        printResult();
-    }
-
-    private static void simulation() {
-        for (int i = 0; i < nominations.length; i++) {
-            solution(i);
-        }
-    }
-
-    private static void solution(int time) {
-        int number = nominations[time];
-        Candidate existCandidate = findEqualNumber(number);
-        if (existCandidate != null) {
-            existCandidate.count++;
-        } else {
-            if (candidateList.size() < CANDIDATE_COUNT) {
-                candidateList.add(new Candidate(number, 1, time));
-            } else {
-                int minIndex = getRemoveCandidateIndex();
-                candidateList.remove(minIndex);
-                candidateList.add(new Candidate(number, 1, time));
-            }
-        }
-    }
-
-    private static int getRemoveCandidateIndex() {
-        int minCount = Integer.MAX_VALUE;
-        int minTime = Integer.MAX_VALUE;
-        int minIndex = -1;
-        for (int i = 0; i < candidateList.size(); i++) {
-            Candidate candidate = candidateList.get(i);
-            if (candidate.count < minCount) {
-                minCount = candidate.count;
-                minTime = candidate.time;
-                minIndex = i;
-            } else if (candidate.count == minCount) {
-                if (candidate.time < minTime) {
-                    minTime = candidate.time;
-                    minIndex = i;
-                }
-            }
-        }
-        return minIndex;
-    }
-
-    private static Candidate findEqualNumber(int number) {
-        for (Candidate candidate : candidateList) {
-            if (candidate.number == number) {
-                return candidate;
-            }
-        }
-        return null;
-    }
-
-    private static void printResult() {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (Candidate candidate : candidateList) {
-            stringBuilder.append(candidate.number).append(" ");
-        }
-        System.out.println(stringBuilder);
-    }
-
-    private static void init() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        CANDIDATE_COUNT = Integer.parseInt(bufferedReader.readLine());
+        int target = Integer.parseInt(bufferedReader.readLine());
         bufferedReader.readLine();
-        nominations = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] votes = Arrays.stream(bufferedReader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        System.out.print(solution(target, votes));
+    }
+
+    private static String solution(int target, int[] votes) {
+        PriorityQueue<Candidate> priorityQueue = new PriorityQueue<>(
+                (o1, o2) -> {
+                    if (o1.recommendCount == o2.recommendCount) {
+                        return o2.time - o1.time;
+                    }
+                    return o1.recommendCount - o2.recommendCount;
+                }
+        );
+
+        for (int vote : votes) {
+            Optional<Candidate> candidate = findCandidate(priorityQueue, vote);
+
+            if (candidate.isPresent()) {
+                candidate.get().recommend();
+            } else {
+                if (priorityQueue.size() == target) {
+                    priorityQueue.poll();
+                }
+                priorityQueue.add(new Candidate(vote));
+            }
+
+            updateCandidateTimes(priorityQueue);
+        }
+
+        return getSortedCandidate(priorityQueue);
+    }
+
+    private static void updateCandidateTimes(PriorityQueue<Candidate> priorityQueue) {
+        priorityQueue.forEach(Candidate::updateTime);
+        PriorityQueue<Candidate> temp = new PriorityQueue<>(priorityQueue);
+        priorityQueue.clear();
+        priorityQueue.addAll(temp);
+    }
+
+    private static String getSortedCandidate(PriorityQueue<Candidate> priorityQueue) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        priorityQueue.stream()
+                .sorted(Comparator.comparingInt(Candidate::getNumber))
+                .forEach(candidate -> stringBuilder.append(candidate.getNumber()).append(" "));
+
+        return stringBuilder.toString().trim();
+    }
+
+    private static Optional<Candidate> findCandidate(
+            PriorityQueue<Candidate> priorityQueue,
+            int vote
+    ) {
+        return priorityQueue.stream()
+                .filter(candidate -> candidate.getNumber() == vote)
+                .findFirst();
     }
 
     static class Candidate {
-        int number;
-        int count;
-        int time;
 
-        public Candidate(int number, int count, int time) {
+        private final int number;
+        private int recommendCount;
+        private int time;
+
+        public Candidate(int number) {
             this.number = number;
-            this.count = count;
-            this.time = time;
+            this.recommendCount = 1;
+            this.time = 0;
+        }
+
+        public void recommend() {
+            recommendCount++;
+        }
+
+        public void updateTime() {
+            time++;
+        }
+
+        public int getNumber() {
+            return number;
         }
     }
 }
