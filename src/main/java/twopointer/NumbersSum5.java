72e2eb1fedb730992fd13a93ebc57d047f8216ea
hyoguoo <hyoguoo@gmail.com>
2024-09-04 13:36:06 +0900

    resolve: 수들의 합 5

--- src/main/java/twopointer/NumbersSum5.java
+++ src/main/java/twopointer/NumbersSum5.java
@@ -15,27 +15,33 @@ import java.io.InputStreamReader;
 public class NumbersSum5 {
 
     public static void main(String[] args) throws IOException {
-        System.out.print(solution(Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine())));
+        System.out.print(
+                solution(
+                        Integer.parseInt(
+                                new BufferedReader(new InputStreamReader(System.in)).readLine()
+                        )
+                )
+        );
     }
 
     private static int solution(int n) {
+        int count = 0;
         int left = 1;
         int right = 1;
-        int sum = 0;
-        int count = 0;
+        int sum = 1;
 
-        while (true) {
-            if (sum >= n) {
+        while (left <= n) {
+            if (sum == n) {
+                count++;
                 sum -= left;
                 left++;
-            } else if (right <= n) {
-                sum += right;
+            } else if (sum < n) {
                 right++;
+                sum += right;
             } else {
-                break;
+                sum -= left;
+                left++;
             }
-
-            if (sum == n) count++;
         }
 
         return count;
