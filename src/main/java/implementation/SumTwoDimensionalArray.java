7b8e450f2372378de08464f46b30d03bc2141fac
hyoguoo <hyoguoo@gmail.com>
2024-09-04 13:49:19 +0900

    resolve: 2차원 배열의 합

--- src/main/java/implementation/SumTwoDimensionalArray.java
+++ src/main/java/implementation/SumTwoDimensionalArray.java
@@ -12,56 +12,112 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class SumTwoDimensionalArray {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int[] info = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int N = info[0];
-        int M = info[1];
-        int[][] array = new int[N][M];
-        for (int n = 0; n < N; n++) {
-            array[n] = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] info = Arrays.stream(bufferedReader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int sizeN = info[0];
+        int sizeM = info[1];
+        int[][] values = new int[sizeN][sizeM];
+        for (int n = 0; n < sizeN; n++) {
+            values[n] = Arrays.stream(bufferedReader.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
         }
 
-        int[][] prefixSum = calculatePrefixSum(N, M, array);
+        int queryCount = Integer.parseInt(bufferedReader.readLine());
 
-        int K = Integer.parseInt(bufferedReader.readLine());
+        Query[] queries = new Query[queryCount];
+        for (int k = 0; k < queryCount; k++) {
+            int[] query = Arrays.stream(bufferedReader.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            queries[k] = new Query(query[0] - 1, query[1] - 1, query[2] - 1, query[3] - 1);
+        }
 
+        System.out.print(solution(values, queries));
+    }
+
+    private static String solution(int[][] values, Query[] queries) {
+        Matrix matrix = new Matrix(values);
         StringBuilder stringBuilder = new StringBuilder();
-        for (int k = 0; k < K; k++) {
-            int[] query = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int result = calculateSum(query, prefixSum);
-            stringBuilder.append(result).append("\n");
-        }
-        System.out.print(stringBuilder);
+
+        Stream.of(queries)
+                .forEach(
+                        query ->
+                                stringBuilder.append(matrix.getSum(query)).append("\n")
+                );
+
+        return stringBuilder.toString().trim();
     }
 
-    private static int[][] calculatePrefixSum(int N, int M, int[][] array) {
-        int[][] prefixSum = new int[N][M];
-        for (int n = 0; n < N; n++) {
-            for (int m = 0; m < M; m++) {
-                prefixSum[n][m] = array[n][m];
-                if (n > 0) prefixSum[n][m] += prefixSum[n - 1][m];
-                if (m > 0) prefixSum[n][m] += prefixSum[n][m - 1];
-                if (n > 0 && m > 0) prefixSum[n][m] -= prefixSum[n - 1][m - 1];
+    static class Matrix {
+
+        private final int[][] values;
+        private final int[][] prefixSum;
+
+        public Matrix(int[][] values) {
+            this.values = values;
+            this.prefixSum = calculatePrefixSum();
+        }
+
+        private int[][] calculatePrefixSum() {
+            int[][] prefixSum = new int[values.length][values[0].length];
+            prefixSum[0][0] = values[0][0];
+
+            for (int n = 1; n < values.length; n++) {
+                prefixSum[n][0] = prefixSum[n - 1][0] + values[n][0];
             }
+            for (int m = 1; m < values[0].length; m++) {
+                prefixSum[0][m] = prefixSum[0][m - 1] + values[0][m];
+            }
+            for (int n = 1; n < values.length; n++) {
+                for (int m = 1; m < values[0].length; m++) {
+                    prefixSum[n][m] =
+                            prefixSum[n - 1][m]
+                                    + prefixSum[n][m - 1]
+                                    - prefixSum[n - 1][m - 1]
+                                    + values[n][m];
+                }
+            }
+
+            return prefixSum;
         }
-        return prefixSum;
+
+        public int getSum(Query query) {
+            int sum = prefixSum[query.n2][query.m2];
+
+            if (query.n1 > 0) {
+                sum -= prefixSum[query.n1 - 1][query.m2];
+            }
+            if (query.m1 > 0) {
+                sum -= prefixSum[query.n2][query.m1 - 1];
+            }
+            if (query.n1 > 0 && query.m1 > 0) {
+                sum += prefixSum[query.n1 - 1][query.m1 - 1];
+            }
+
+            return sum;
+        }
     }
 
-    private static int calculateSum(int[] query, int[][] prefixSum) {
-        int n1 = query[0] - 1;
-        int m1 = query[1] - 1;
-        int n2 = query[2] - 1;
-        int m2 = query[3] - 1;
+    static class Query {
 
-        int result = prefixSum[n2][m2];
-        if (n1 > 0) result -= prefixSum[n1 - 1][m2];
-        if (m1 > 0) result -= prefixSum[n2][m1 - 1];
-        if (n1 > 0 && m1 > 0) result += prefixSum[n1 - 1][m1 - 1];
+        private final int n1;
+        private final int m1;
+        private final int n2;
+        private final int m2;
 
-        return result;
+        public Query(int n1, int m1, int n2, int m2) {
+            this.n1 = n1;
+            this.m1 = m1;
+            this.n2 = n2;
+            this.m2 = m2;
+        }
     }
 }
