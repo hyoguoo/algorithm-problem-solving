7f4394ecdbfb551356f785fcd663fbba5c5c2731
hyoguoo <hyoguoo@gmail.com>
2024-09-05 11:45:56 +0900

    resolve: 재귀함수가 뭔가요?

--- src/main/java/implementation/WhatIsRecursionFunction.java
+++ src/main/java/implementation/WhatIsRecursionFunction.java
@@ -18,27 +18,32 @@ public class WhatIsRecursionFunction {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int number = Integer.parseInt(bufferedReader.readLine());
+        solution(Integer.parseInt(bufferedReader.readLine()));
 
-        appendString("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.", 0);
-        recursion(number, 0);
-        System.out.println(stringBuilder);
+        System.out.print(stringBuilder.toString().trim());
     }
 
-    private static void recursion(int number, int depth) {
-        appendString("\"재귀함수가 뭔가요?\"", depth);
-        if (depth == number) appendString("\"재귀함수는 자기 자신을 호출하는 함수라네\"", depth);
-        else {
-            appendString("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.", depth);
-            appendString("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.", depth);
-            appendString("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"", depth);
-            recursion(number, depth + 1);
+    private static void solution(int depth) {
+        appendString("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.", 0);
+        recursion(depth, 0);
+    }
+
+    private static void recursion(int depth, int currentDepth) {
+        appendString("\"재귀함수가 뭔가요?\"", currentDepth);
+        if (currentDepth == depth) {
+            appendString("\"재귀함수는 자기 자신을 호출하는 함수라네\"", currentDepth);
+        } else {
+            appendString("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.", currentDepth);
+            appendString("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.", currentDepth);
+            appendString("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"", currentDepth);
+            recursion(depth, currentDepth + 1);
         }
-        appendString("라고 답변하였지.", depth);
+        appendString("라고 답변하였지.", currentDepth);
     }
 
     private static void appendString(String string, int depth) {
-        stringBuilder.append("____".repeat(depth));
-        stringBuilder.append(string).append("\n");
+        stringBuilder.append("____".repeat(depth))
+                .append(string)
+                .append("\n");
     }
 }
