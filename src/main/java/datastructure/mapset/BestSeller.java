1ac6e18bb7479c0dd26bb73d3759f633bb9088d1
hyoguoo <hyoguoo@gmail.com>
2024-09-06 10:41:21 +0900

    resolve: 베스트셀러

--- src/main/java/datastructure/mapset/BestSeller.java
+++ src/main/java/datastructure/mapset/BestSeller.java
@@ -11,37 +11,52 @@ package datastructure.mapset;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class BestSeller {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int length = Integer.parseInt(bufferedReader.readLine());
-        Map<String, Integer> map = new HashMap<>();
+        int n = Integer.parseInt(bufferedReader.readLine());
 
-        for (int i = 0; i < length; i++) {
-            String input = bufferedReader.readLine();
-            map.put(input, map.getOrDefault(input, 0) + 1);
+        String[] books = new String[n];
+        for (int i = 0; i < n; i++) {
+            books[i] = bufferedReader.readLine();
         }
 
-        String result = getResult(map);
-        System.out.println(result);
+        System.out.print(solution(books));
     }
 
-    private static String getResult(Map<String, Integer> map) {
-        String result = "";
-        int max = 0;
+    private static String solution(String[] books) {
+        Map<String, Integer> bookCount = getBookCount(books);
 
-        for (String key : map.keySet()) {
-            if (map.get(key) > max) {
-                max = map.get(key);
-                result = key;
-            } else if (map.get(key) == max) {
-                if (result.compareTo(key) > 0) result = key;
-            }
+        return bookCount
+                .entrySet()
+                .stream()
+                .max(entryComparator())
+                .orElseThrow()
+                .getKey();
+    }
+
+    private static Map<String, Integer> getBookCount(String[] books) {
+        Map<String, Integer> bookCount = new HashMap<>();
+
+        for (String book : books) {
+            bookCount.put(book, bookCount.getOrDefault(book, 0) + 1);
         }
-        return result;
+
+        return bookCount;
+    }
+
+    private static Comparator<Entry<String, Integer>> entryComparator() {
+        return (entry1, entry2) -> {
+            if (entry1.getValue().equals(entry2.getValue())) {
+                return entry2.getKey().compareTo(entry1.getKey());
+            }
+            return entry1.getValue().compareTo(entry2.getValue());
+        };
     }
 }
