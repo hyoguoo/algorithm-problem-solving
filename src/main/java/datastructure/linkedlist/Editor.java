982a03b51c7a5012d3009d4dc190cb46823efa13
hyoguoo <hyoguoo@gmail.com>
2024-09-25 10:50:01 +0900

    resolve: 에디터

--- src/main/java/datastructure/linkedlist/Editor.java
+++ src/main/java/datastructure/linkedlist/Editor.java
@@ -11,57 +11,135 @@ package datastructure.linkedlist;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+import java.util.Optional;
 
 public class Editor {
 
-    static final Stack<Character> leftStack = new Stack<>();
-    static final Stack<Character> rightStack = new Stack<>();
-    static final char LEFT = 'L';
-    static final char RIGHT = 'D';
-    static final char DELETE = 'B';
-    static final char INSERT = 'P';
-
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        char[] input = bufferedReader.readLine().toCharArray();
-        for (char c : input) leftStack.add(c);
-        int commandCount = Integer.parseInt(bufferedReader.readLine());
+        String text = bufferedReader.readLine();
 
-        while (commandCount-- > 0) {
-            char[] command = bufferedReader.readLine().toCharArray();
-            action(command);
+        int queryCount = Integer.parseInt(bufferedReader.readLine());
+        List<Query> queryList = new ArrayList<>();
+
+        for (int i = 0; i < queryCount; i++) {
+            String[] query = bufferedReader.readLine().split(" ");
+            if (query.length == 1) {
+                queryList.add(new Query(Command.of(query[0])));
+            } else {
+                queryList.add(new Query(Command.of(query[0]), query[1].charAt(0)));
+            }
         }
 
-        printResult();
+        System.out.print(solution(text, queryList));
     }
 
-    private static void action(char[] command) {
-        switch (command[0]) {
-            case LEFT:
-                if (leftStack.isEmpty()) break;
-                rightStack.push(leftStack.pop());
-                break;
-            case RIGHT:
-                if (rightStack.isEmpty()) break;
-                leftStack.push(rightStack.pop());
-                break;
-            case DELETE:
-                if (leftStack.isEmpty()) break;
-                leftStack.pop();
-                break;
-            case INSERT:
-                leftStack.push(command[2]);
-                break;
-            default:
-                throw new IllegalArgumentException();
+    private static String solution(String text, List<Query> queryList) {
+        Text textEditor = new Text(text);
+
+        queryList.forEach(textEditor::executeQuery);
+
+        return textEditor.toString();
+    }
+
+    enum Command {
+        LEFT("L"),
+        RIGHT("D"),
+        DELETE("B"),
+        INSERT("P");
+
+        private final String value;
+
+        Command(String value) {
+            this.value = value;
+        }
+
+        public static Command of(String value) {
+            return Arrays.stream(values())
+                    .filter(command -> command.value.equals(value))
+                    .findFirst()
+                    .orElseThrow(IllegalArgumentException::new);
         }
     }
 
-    private static void printResult() {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (Character character : leftStack) stringBuilder.append(character);
-        while (!rightStack.isEmpty()) stringBuilder.append(rightStack.pop());
-        System.out.print(stringBuilder);
+    static class Text {
+
+        private final Deque<Character> leftStack;
+        private final Deque<Character> rightStack;
+
+        public Text(String text) {
+            this.leftStack = new ArrayDeque<>();
+            this.rightStack = new ArrayDeque<>();
+
+            for (char c : text.toCharArray()) {
+                leftStack.push(c);
+            }
+        }
+
+        public void executeQuery(Query query) {
+            switch (query.getCommand()) {
+                case LEFT:
+                    if (!leftStack.isEmpty()) {
+                        rightStack.push(leftStack.pop());
+                    }
+                    break;
+                case RIGHT:
+                    if (!rightStack.isEmpty()) {
+                        leftStack.push(rightStack.pop());
+                    }
+                    break;
+                case DELETE:
+                    if (!leftStack.isEmpty()) {
+                        leftStack.pop();
+                    }
+                    break;
+                case INSERT:
+                    leftStack.push(query.getValue());
+                    break;
+            }
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            leftStack.descendingIterator()
+                    .forEachRemaining(stringBuilder::append);
+
+            rightStack.iterator()
+                    .forEachRemaining(stringBuilder::append);
+
+            return stringBuilder.toString().trim();
+        }
+    }
+
+    static class Query {
+
+        private final Command command;
+        private final Character value;
+
+        public Query(Command command) {
+            this.command = command;
+            this.value = null;
+        }
+
+        public Query(Command command, Character value) {
+            this.command = command;
+            this.value = value;
+        }
+
+        public Command getCommand() {
+            return command;
+        }
+
+        public Character getValue() {
+            return Optional.ofNullable(value)
+                    .orElseThrow(IllegalArgumentException::new);
+        }
     }
 }
