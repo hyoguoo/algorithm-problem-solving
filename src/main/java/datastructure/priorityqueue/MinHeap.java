4dc965bc9b2e00a4102de82307ce6d9e411d4ffd
hyoguoo <hyoguoo@gmail.com>
2024-09-26 09:27:25 +0900

    resolve: 최소 힙

--- src/main/java/datastructure/priorityqueue/MinHeap.java
+++ src/main/java/datastructure/priorityqueue/MinHeap.java
@@ -11,30 +11,70 @@ package datastructure.priorityqueue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class MinHeap {
 
-    static PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int length = Integer.parseInt(bufferedReader.readLine());
+        int n = Integer.parseInt(bufferedReader.readLine());
 
-        for (int i = 0; i < length; i++) {
-            int number = Integer.parseInt(bufferedReader.readLine());
-            int result = solution(number);
-            if (result >= 0) System.out.println(result);
+        int[] queries = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            queries[i] = Integer.parseInt(bufferedReader.readLine());
         }
+
+        System.out.print(solution(queries));
     }
 
-    private static int solution(int number) {
-        if (number == 0) {
-            if (priorityQueue.isEmpty()) return 0;
-            return priorityQueue.poll();
-        } else {
-            priorityQueue.add(number);
-            return -1;
+    private static String solution(int[] queries) {
+        PriorityQueueImpl priorityQueue = new PriorityQueueImpl();
+        Arrays.stream(queries)
+                .forEach(priorityQueue::executeQuery);
+        return priorityQueue.formatPolledList();
+    }
+
+    static class PriorityQueueImpl {
+
+        private static final int POLL_SIGNAL = 0;
+        private static final int EMPTY_VALUE = 0;
+        private final PriorityQueue<Integer> queue;
+        private final List<Integer> polledList;
+
+        public PriorityQueueImpl() {
+            this.queue = new PriorityQueue<>();
+            this.polledList = new ArrayList<>();
+        }
+
+        public void executeQuery(int query) {
+            if (query == POLL_SIGNAL) {
+                poll();
+            } else {
+                add(query);
+            }
+        }
+
+        private void poll() {
+            if (queue.isEmpty()) {
+                polledList.add(EMPTY_VALUE);
+            } else {
+                polledList.add(queue.poll());
+            }
+        }
+
+        private void add(int query) {
+            queue.add(query);
+        }
+
+        public String formatPolledList() {
+            StringBuilder stringBuilder = new StringBuilder();
+            Arrays.stream(polledList.toArray())
+                    .forEach(e -> stringBuilder.append(e).append("\n"));
+            return stringBuilder.toString().trim();
         }
     }
 }
