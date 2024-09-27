81e2c8d7fe21e25a4af9f5931dda55386ccca8c9
hyoguoo <hyoguoo@gmail.com>
2024-09-27 16:22:17 +0900

    resolve: 양치기 꿍

--- src/main/java/graph/bfs/ShepherdKung.java
+++ src/main/java/graph/bfs/ShepherdKung.java
@@ -17,87 +17,144 @@ import java.util.Queue;
 
 public class ShepherdKung {
 
-    static final char WALL = '#';
-    static final char SHEEP = 'k';
-    static final char WOLF = 'v';
-    static final char VISITED = 'x';
-    static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    static final Count count = new Count();
-
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int[] info = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int n = info[0];
-        int m = info[1];
-        char[][] graph = new char[n][m];
-
-        for (int i = 0; i < n; i++) {
-            graph[i] = bufferedReader.readLine().toCharArray();
-        }
-
-        solution(graph);
+        Symbol[][] graph = parseGraph();
+        System.out.print(solution(graph));
     }
 
-    private static void solution(char[][] graph) {
+    private static Symbol[][] parseGraph() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[] info = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt)
+                .toArray();
+        int n = info[0];
+        int m = info[1];
+        Symbol[][] graph = new Symbol[n][m];
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = Arrays.stream(bufferedReader.readLine().split(""))
+                    .map(s -> Symbol.fromChar(s.charAt(0)))
+                    .toArray(Symbol[]::new);
+        }
+
+        return graph;
+    }
+
+    private static String solution(Symbol[][] graph) {
+        Count count = new Count();
+
         for (int n = 0; n < graph.length; n++) {
             for (int m = 0; m < graph[n].length; m++) {
-                if (graph[n][m] != WALL &&
-                    graph[n][m] != VISITED) bfs(graph, n, m);
+                if (graph[n][m] != Symbol.WALL &&
+                        graph[n][m] != Symbol.VISITED) {
+                    bfs(graph, new Coordinate(n, m), count);
+                }
             }
         }
 
-        System.out.println(count.sheepCount + " " + count.wolfCount);
+        return count.toString();
     }
 
-    private static void bfs(char[][] graph, int startN, int startM) {
+    private static void bfs(Symbol[][] graph, Coordinate start, Count count) {
         Queue<Coordinate> queue = new LinkedList<>();
-        queue.add(new Coordinate(startN, startM));
+        queue.add(start);
         int sheepCount = 0;
         int wolfCount = 0;
 
-        if (graph[startN][startM] == SHEEP) sheepCount++;
-        if (graph[startN][startM] == WOLF) wolfCount++;
-        graph[startN][startM] = VISITED;
+        if (graph[start.n][start.m] == Symbol.SHEEP) {
+            sheepCount++;
+        }
+        if (graph[start.n][start.m] == Symbol.WOLF) {
+            wolfCount++;
+        }
+        graph[start.n][start.m] = Symbol.VISITED;
 
         while (!queue.isEmpty()) {
-            Coordinate current = queue.poll();
+            Coordinate coordinate = queue.poll();
 
-            for (int[] direction : DIRECTIONS) {
-                int nextN = current.n + direction[0];
-                int nextM = current.m + direction[1];
+            for (Direction direction : Direction.values()) {
+                Coordinate nextCoordinate = coordinate.move(direction);
 
-                if (!isInBound(graph, nextN, nextM) ||
-                    graph[nextN][nextM] == WALL ||
-                    graph[nextN][nextM] == VISITED) continue;
+                if (!isInBound(graph, nextCoordinate) ||
+                        graph[nextCoordinate.n][nextCoordinate.m] == Symbol.WALL ||
+                        graph[nextCoordinate.n][nextCoordinate.m] == Symbol.VISITED) {
+                    continue;
+                }
 
-                if (graph[nextN][nextM] == SHEEP) sheepCount++;
-                if (graph[nextN][nextM] == WOLF) wolfCount++;
-                graph[nextN][nextM] = VISITED;
-                queue.add(new Coordinate(nextN, nextM));
+                if (graph[nextCoordinate.n][nextCoordinate.m] == Symbol.SHEEP) {
+                    sheepCount++;
+                }
+                if (graph[nextCoordinate.n][nextCoordinate.m] == Symbol.WOLF) {
+                    wolfCount++;
+                }
+                graph[nextCoordinate.n][nextCoordinate.m] = Symbol.VISITED;
+                queue.add(nextCoordinate);
             }
         }
 
         count.updateCount(sheepCount, wolfCount);
     }
 
-    private static boolean isInBound(char[][] graph, int nextN, int nextM) {
-        return 0 <= nextN && nextN < graph.length &&
-               0 <= nextM && nextM < graph[nextN].length;
+    private static boolean isInBound(Symbol[][] graph, Coordinate coordinate) {
+        return 0 <= coordinate.n && coordinate.n < graph.length &&
+                0 <= coordinate.m && coordinate.m < graph[coordinate.n].length;
+    }
+
+    enum Direction {
+        UP(-1, 0),
+        DOWN(1, 0),
+        LEFT(0, -1),
+        RIGHT(0, 1);
+
+        private final int n;
+        private final int m;
+
+        Direction(int n, int m) {
+            this.n = n;
+            this.m = m;
+        }
+
+    }
+
+    enum Symbol {
+        WALL('#'),
+        SHEEP('k'),
+        WOLF('v'),
+        VISITED('x'),
+        EMPTY('.');
+
+        private final char value;
+
+        Symbol(char value) {
+            this.value = value;
+        }
+
+        public static Symbol fromChar(char ch) {
+            return Arrays.stream(Symbol.values())
+                    .filter(symbol -> symbol.value == ch)
+                    .findAny()
+                    .orElseThrow(IllegalArgumentException::new);
+        }
     }
 
     static class Coordinate {
-        int n;
-        int m;
+
+        private final int n;
+        private final int m;
 
         public Coordinate(int n, int m) {
             this.n = n;
             this.m = m;
         }
+
+        public Coordinate move(Direction direction) {
+            return new Coordinate(this.n + direction.n, this.m + direction.m);
+        }
     }
 
     static class Count {
-        int sheepCount;
-        int wolfCount;
+
+        private int sheepCount;
+        private int wolfCount;
 
         public Count() {
             this.sheepCount = 0;
@@ -105,8 +162,16 @@ public class ShepherdKung {
         }
 
         public void updateCount(int sheepCount, int wolfCount) {
-            if (sheepCount > wolfCount) this.sheepCount += sheepCount;
-            else this.wolfCount += wolfCount;
+            if (sheepCount > wolfCount) {
+                this.sheepCount += sheepCount;
+            } else {
+                this.wolfCount += wolfCount;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return this.sheepCount + " " + this.wolfCount;
         }
     }
 }
