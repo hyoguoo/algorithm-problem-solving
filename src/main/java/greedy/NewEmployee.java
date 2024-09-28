88b54428d2cb253541e79e0160741d50c55cafd6
hyoguoo <hyoguoo@gmail.com>
2024-09-28 10:13:33 +0900

    resolve: 신입 사원

--- src/main/java/greedy/NewEmployee.java
+++ src/main/java/greedy/NewEmployee.java
@@ -11,47 +11,59 @@ package greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class NewEmployee {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int caseCount = Integer.parseInt(bufferedReader.readLine());
-        for (int count = 0; count < caseCount; count++) {
+        int testCount = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (testCount-- > 0) {
             int employeeCount = Integer.parseInt(bufferedReader.readLine());
-            List<Employee> employees = new ArrayList<>();
+            Employee[] employees = new Employee[employeeCount];
             for (int i = 0; i < employeeCount; i++) {
-                int[] employeeInfo = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                employees.add(new Employee(employeeInfo[0], employeeInfo[1]));
+                int[] employeeInfo = Arrays.stream(bufferedReader.readLine().split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+                employees[i] = (new Employee(employeeInfo[0], employeeInfo[1]));
             }
-            System.out.println(solution(employees));
+            stringBuilder.append(solution(employees)).append("\n");
         }
+
+        System.out.print(stringBuilder.toString().trim());
     }
 
-    private static int solution(List<Employee> employees) {
-        employees.sort(Comparator.comparingInt(o -> o.document));
+    private static int solution(Employee[] employees) {
+        Arrays.sort(employees, Comparator.comparingInt(o -> o.document));
         int notHiredCount = 0;
-        int interviewLowerBound = employees.get(0).interview;
+        int interviewLowerBound = employees[0].interview;
 
         for (Employee employee : employees) {
-            if (employee.interview > interviewLowerBound) notHiredCount++;
-            else interviewLowerBound = employee.interview;
+            if (employee.isHigherInterviewScore(interviewLowerBound)) {
+                notHiredCount++;
+            } else {
+                interviewLowerBound = employee.interview;
+            }
         }
 
-        return employees.size() - notHiredCount;
+        return employees.length - notHiredCount;
     }
 
     static class Employee {
-        int document;
-        int interview;
+
+        private final int document;
+        private final int interview;
 
         public Employee(int document, int interview) {
             this.document = document;
             this.interview = interview;
+        }
+
+        public boolean isHigherInterviewScore(int interviewScore) {
+            return this.interview > interviewScore;
         }
     }
 }
