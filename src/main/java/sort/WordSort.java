0b824a2ca852b2516e6805dcb6284cb0cc823247
hyoguoo <hyoguoo@gmail.com>
2024-09-28 10:21:09 +0900

    resolve: 단어 정렬

--- src/main/java/sort/WordSort.java
+++ src/main/java/sort/WordSort.java
@@ -11,30 +11,39 @@ package sort;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class WordSort {
-    static List<String> stringList = new ArrayList<>();
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int length = Integer.parseInt(bufferedReader.readLine());
+        int wordCount = Integer.parseInt(bufferedReader.readLine());
 
-        Comparator<String> customComparator = new Comparator<>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if (o1.length() != o2.length()) return o1.length() - o2.length();
-                else return o1.compareTo(o2);
+        String[] words = new String[wordCount];
+        for (int i = 0; i < wordCount; i++) {
+            words[i] = bufferedReader.readLine();
+        }
+
+        System.out.print(solution(words));
+    }
+
+    private static String solution(String[] words) {
+        return Arrays.stream(words)
+                .distinct()
+                .sorted(getWordComparator())
+                .collect(Collectors.joining("\n"));
+    }
+
+    private static Comparator<String> getWordComparator() {
+        return (o1, o2) -> {
+            if (o1.length() != o2.length()) {
+                return o1.length() - o2.length();
+            } else {
+                return o1.compareTo(o2);
             }
         };
-
-        for (int i = 0; i < length; i++) stringList.add(bufferedReader.readLine());
-        List<String> uniqueStringList = stringList.stream().distinct().collect(Collectors.toList());
-        uniqueStringList.sort(customComparator);
-
-        for (String string : uniqueStringList) System.out.println(string);
     }
 }
