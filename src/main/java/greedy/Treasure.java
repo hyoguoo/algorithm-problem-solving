a78e112fa89f830fded1a4d54b0345cfdbc3c099
hyoguoo <hyoguoo@gmail.com>
2024-09-28 09:11:56 +0900

    resolve: 보물

--- src/main/java/greedy/Treasure.java
+++ src/main/java/greedy/Treasure.java
@@ -12,28 +12,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Treasure {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         bufferedReader.readLine();
-        int[] numbers1 = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] numbers2 = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] numbers1 = Arrays.stream(bufferedReader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] numbers2 = Arrays.stream(bufferedReader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-        System.out.println(solution(numbers1, numbers2));
+        System.out.print(solution(numbers1, numbers2));
     }
 
     private static int solution(int[] numbers1, int[] numbers2) {
         Arrays.sort(numbers1);
         Arrays.sort(numbers2);
 
-        int sum = 0;
-
-        for (int i = 0; i < numbers1.length; i++) {
-            sum += numbers1[i] * numbers2[numbers2.length - 1 - i];
-        }
-
-        return sum;
+        return IntStream.range(0, numbers1.length)
+                .map(i -> numbers1[i] * numbers2[numbers2.length - 1 - i])
+                .sum();
     }
 }
