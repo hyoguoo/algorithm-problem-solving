790dcdffae0bc17dab5f4f295338c70379879bab
hyoguoo <hyoguoo@gmail.com>
2024-09-30 07:37:21 +0900

    resolve: 나이트의 이동

--- src/main/java/graph/bfs/KnightMoves.java
+++ src/main/java/graph/bfs/KnightMoves.java
@@ -17,59 +17,106 @@ import java.util.Queue;
 
 public class KnightMoves {
 
-    final static int[][] DIRECTIONS = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
-    final static int NOT_VISITED = 0;
+    private static final int NOT_VISITED = -1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder stringBuilder = new StringBuilder();
         int testCount = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
 
         while (testCount-- > 0) {
-            int N = Integer.parseInt(bufferedReader.readLine());
-            int[] startInfo = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int[] endInfo = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            stringBuilder.append(solution(N, new Coordinate(startInfo[0], startInfo[1]), new Coordinate(endInfo[0], endInfo[1]))).append("\n");
+            int sizeN = Integer.parseInt(bufferedReader.readLine());
+            int[] startInfo = Arrays.stream(bufferedReader.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            int[] endInfo = Arrays.stream(bufferedReader.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            stringBuilder
+                    .append(
+                            solution(
+                                    new Coordinate(startInfo[0], startInfo[1]),
+                                    new Coordinate(endInfo[0], endInfo[1]),
+                                    sizeN
+                            )
+                    )
+                    .append("\n");
         }
 
-        System.out.println(stringBuilder);
+        System.out.print(stringBuilder.toString().trim());
     }
 
-    private static int solution(int N, Coordinate start, Coordinate end) {
+    private static int solution(Coordinate startCoordinate, Coordinate endCoordinate, int sizeN) {
         Queue<Coordinate> queue = new LinkedList<>();
-        queue.add(start);
-        int[][] distance = new int[N][N];
+        int[][] distance = initDistance(sizeN);
+        queue.add(startCoordinate);
+        distance[startCoordinate.n][startCoordinate.m] = 0;
 
         while (!queue.isEmpty()) {
-            Coordinate current = queue.poll();
-
-            if (current.n == end.n && current.m == end.m) return distance[current.n][current.m];
-
-            for (int[] direction : DIRECTIONS) {
-                int nextN = current.n + direction[0];
-                int nextM = current.m + direction[1];
-
-                if (isInBound(N, nextN, nextM) && distance[nextN][nextM] == NOT_VISITED) {
-                    distance[nextN][nextM] = distance[current.n][current.m] + 1;
-                    queue.add(new Coordinate(nextN, nextM));
+            Coordinate currentCoordinate = queue.poll();
+            if (currentCoordinate.n == endCoordinate.n && currentCoordinate.m == endCoordinate.m) {
+                return distance[currentCoordinate.n][currentCoordinate.m];
+            }
+            for (Direction direction : Direction.values()) {
+                Coordinate nextCoordinate = currentCoordinate.nextCoordinate(direction);
+                if (!isInBound(sizeN, nextCoordinate)) {
+                    continue;
+                }
+                if (distance[nextCoordinate.n][nextCoordinate.m] == NOT_VISITED) {
+                    distance[nextCoordinate.n][nextCoordinate.m] =
+                            distance[currentCoordinate.n][currentCoordinate.m] + 1;
+                    queue.add(nextCoordinate);
                 }
             }
         }
 
-        throw new IllegalArgumentException();
+        return -1;
     }
 
-    private static boolean isInBound(int N, int n, int m) {
-        return 0 <= n && n < N && 0 <= m && m < N;
+    private static int[][] initDistance(int sizeN) {
+        int[][] distance = new int[sizeN][sizeN];
+        Arrays.stream(distance)
+                .forEach(row -> Arrays.fill(row, NOT_VISITED));
+        return distance;
+    }
+
+    private static boolean isInBound(int sizeN, Coordinate coordinate) {
+        return 0 <= coordinate.n && coordinate.n < sizeN
+                && 0 <= coordinate.m && coordinate.m < sizeN;
+    }
+
+    enum Direction {
+        UP_LEFT(-2, -1),
+        UP_RIGHT(-2, 1),
+        RIGHT_UP(-1, 2),
+        RIGHT_DOWN(1, 2),
+        DOWN_RIGHT(2, 1),
+        DOWN_LEFT(2, -1),
+        LEFT_DOWN(1, -2),
+        LEFT_UP(-1, -2);
+
+        private final int n;
+        private final int m;
+
+        Direction(int n, int m) {
+            this.n = n;
+            this.m = m;
+        }
     }
 
     static class Coordinate {
-        int n;
-        int m;
+
+        private final int n;
+        private final int m;
 
         public Coordinate(int n, int m) {
             this.n = n;
             this.m = m;
+        }
+
+        public Coordinate nextCoordinate(Direction direction) {
+            return new Coordinate(n + direction.n, m + direction.m);
         }
     }
 }
