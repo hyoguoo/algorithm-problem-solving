4b86d47e6ec8f5f2ab530bdb08afe69beea85adf
hyoguoo <hyoguoo@gmail.com>
2024-09-30 09:18:51 +0900

    solve: 토마토

--- src/main/java/graph/bfs/Tomato3D.java
+++ src/main/java/graph/bfs/Tomato3D.java
@@ -17,79 +17,171 @@ import java.util.Queue;
 
 public class Tomato3D {
 
-    static int M;
-    static int N;
-    static int H;
-    static int[][][] box;
-    static Queue<TomatoCoordinate> tomatoCoordinates = new LinkedList<>();
-    static int[] dx = {0, 0, 0, 0, 1, -1};
-    static int[] dy = {0, 0, 1, -1, 0, 0};
-    static int[] dz = {1, -1, 0, 0, 0, 0};
-
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int[] infos = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        M = infos[0];
-        N = infos[1];
-        H = infos[2];
-        box = new int[H][N][M];
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < N; j++) {
-                int[] tomatoList = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                for (int k = 0; k < M; k++) {
-                    box[i][j][k] = tomatoList[k];
-                    if (box[i][j][k] == 1) tomatoCoordinates.add(new TomatoCoordinate(i, j, k));
+        int[] info = Arrays.stream(bufferedReader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int sizeM = info[0];
+        int sizeN = info[1];
+        int sizeH = info[2];
+
+        Symbol[][][] tomatoes = new Symbol[sizeH][sizeN][sizeM];
+
+        for (int h = 0; h < sizeH; h++) {
+            for (int n = 0; n < sizeN; n++) {
+                tomatoes[h][n] = Arrays.stream(bufferedReader.readLine().split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .mapToObj(Symbol::of)
+                        .toArray(Symbol[]::new);
+            }
+        }
+
+        System.out.print(solution(tomatoes));
+    }
+
+    private static int solution(Symbol[][][] tomatoes) {
+        TomatoBox tomatoBox = new TomatoBox(tomatoes);
+        tomatoBox.ripenTomatoes();
+
+        return tomatoBox.getDay();
+    }
+
+    enum Symbol {
+        EMPTY(-1),
+        RIPENED_TOMATO(1),
+        UNRIPENED_TOMATO(0);
+
+        private final int value;
+
+        Symbol(int value) {
+            this.value = value;
+        }
+
+        public static Symbol of(int value) {
+            return Arrays.stream(values())
+                    .filter(symbol -> symbol.value == value)
+                    .findFirst()
+                    .orElseThrow();
+        }
+    }
+
+    enum Direction {
+        UP(-1, 0, 0),
+        DOWN(1, 0, 0),
+        LEFT(0, -1, 0),
+        RIGHT(0, 1, 0),
+        FRONT(0, 0, 1),
+        BACK(0, 0, -1);
+
+        private final int h;
+        private final int n;
+        private final int m;
+
+        Direction(int h, int n, int m) {
+            this.h = h;
+            this.n = n;
+            this.m = m;
+        }
+    }
+
+    static class TomatoBox {
+
+        private static final int NOT_VISITED = -1;
+        private static final int NOT_ALL_RIPENED = -1;
+
+        private final Symbol[][][] tomatoes;
+        private final int[][][] distance;
+        private final Queue<Coordinate> queue;
+
+        public TomatoBox(Symbol[][][] tomatoes) {
+            this.tomatoes = tomatoes;
+            this.distance = new int[tomatoes.length][tomatoes[0].length][tomatoes[0][0].length];
+            this.queue = new LinkedList<>();
+            for (int h = 0; h < this.tomatoes.length; h++) {
+                for (int n = 0; n < this.tomatoes[0].length; n++) {
+                    for (int m = 0; m < this.tomatoes[0][0].length; m++) {
+                        if (this.tomatoes[h][n][m] == Symbol.RIPENED_TOMATO) {
+                            queue.add(new Coordinate(h, n, m));
+                            distance[h][n][m] = 0;
+                        } else {
+                            distance[h][n][m] = NOT_VISITED;
+                        }
+                    }
                 }
             }
         }
 
-        solution();
-        System.out.println(getMaxValue(box));
-    }
+        public void ripenTomatoes() {
+            while (!queue.isEmpty()) {
+                Coordinate coordinate = queue.poll();
+                ripenAdjacentTomatoes(coordinate);
+            }
+        }
 
-    private static int getMaxValue(int[][][] box) {
-        int max = Integer.MIN_VALUE;
-        for (int[][] boxes : box) {
-            for (int[] tomatoes : boxes) {
-                for (int tomato : tomatoes) {
-                    if (tomato == 0) return -1;
-                    if (max < tomato) max = tomato;
+        private void ripenAdjacentTomatoes(Coordinate coordinate) {
+            for (Direction direction : Direction.values()) {
+                Coordinate nextCoordinate = coordinate.nextCoordinate(direction);
+                if (canRipen(nextCoordinate)) {
+                    ripenAndEnqueue(coordinate, nextCoordinate);
                 }
             }
         }
-        return max - 1;
-    }
 
-    private static void solution() {
-        while (!tomatoCoordinates.isEmpty()) {
-            TomatoCoordinate tomatoCoordinate = tomatoCoordinates.poll();
-            for (int d = 0; d < 6; d++) {
-                int nx = tomatoCoordinate.x + dx[d];
-                int ny = tomatoCoordinate.y + dy[d];
-                int nz = tomatoCoordinate.z + dz[d];
-                traversal(tomatoCoordinate, nx, ny, nz);
-            }
+        private boolean canRipen(Coordinate coordinate) {
+            return coordinate.isInBound(tomatoes) &&
+                    tomatoes[coordinate.h][coordinate.n][coordinate.m] == Symbol.UNRIPENED_TOMATO;
+        }
+
+        private void ripenAndEnqueue(Coordinate coordinate, Coordinate nextCoordinate) {
+            tomatoes[nextCoordinate.h][nextCoordinate.n][nextCoordinate.m] = Symbol.RIPENED_TOMATO;
+            distance[nextCoordinate.h][nextCoordinate.n][nextCoordinate.m] =
+                    distance[coordinate.h][coordinate.n][coordinate.m] + 1;
+            queue.add(nextCoordinate);
+        }
+
+        public int getDay() {
+            return isAllTomatoRipened()
+                    ? findMaxDistance()
+                    : NOT_ALL_RIPENED;
+        }
+
+        private boolean isAllTomatoRipened() {
+            return Arrays.stream(tomatoes)
+                    .flatMap(Arrays::stream)
+                    .flatMap(Arrays::stream)
+                    .noneMatch(symbol -> symbol == Symbol.UNRIPENED_TOMATO);
+        }
+
+        private int findMaxDistance() {
+            return Arrays.stream(distance)
+                    .flatMap(Arrays::stream)
+                    .flatMapToInt(Arrays::stream)
+                    .max()
+                    .orElseThrow();
         }
     }
 
-    private static void traversal(TomatoCoordinate tomatoCoordinate, int nx, int ny, int nz) {
-        if (0 <= nx && nx < H && 0 <= ny && ny < N && 0 <= nz && nz < M) {
-            if (box[nx][ny][nz] == 0) {
-                box[nx][ny][nz] = box[tomatoCoordinate.x][tomatoCoordinate.y][tomatoCoordinate.z] + 1;
-                tomatoCoordinates.add(new TomatoCoordinate(nx, ny, nz));
-            }
+    static class Coordinate {
+
+        private final int h;
+        private final int n;
+        private final int m;
+
+        public Coordinate(int h, int n, int m) {
+            this.h = h;
+            this.n = n;
+            this.m = m;
         }
-    }
-}
 
-class TomatoCoordinate {
-    int x;
-    int y;
-    int z;
+        public Coordinate nextCoordinate(Direction direction) {
+            return new Coordinate(h + direction.m, n + direction.h, m + direction.n);
+        }
 
-    public TomatoCoordinate(int x, int y, int z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        public <T> boolean isInBound(T[][][] board) {
+            return 0 <= h && h < board.length
+                    && 0 <= n && n < board[0].length
+                    && 0 <= m && m < board[0][0].length;
+        }
     }
 }
