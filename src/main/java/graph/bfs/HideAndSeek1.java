a975d8983dd7794dd8881539fd697459ff82ec76
hyoguoo <hyoguoo@gmail.com>
2024-10-01 09:08:46 +0900

    resolve: 숨바꼭질

--- src/main/java/graph/bfs/HideAndSeek1.java
+++ src/main/java/graph/bfs/HideAndSeek1.java
@@ -2,7 +2,7 @@
  * BAEKJOON ONLINE JUDGE
  * https://www.acmicpc.net
  * Problem Number: 1697
- * Cheat Level: 1
+ * Cheat Level: 0
  * Algorithm: Graph / BFS
  */
 
@@ -12,39 +12,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class HideAndSeek1 {
 
+    private static final int MAX = 100_000;
+
     public static void main(String[] args) throws IOException {
-        int[] numbers = Arrays.stream(new BufferedReader(new InputStreamReader(System.in)).readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int start = numbers[0];
-        int end = numbers[1];
-        System.out.println(solution(start, end));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[] numbers = Arrays.stream(bufferedReader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        System.out.print(solution(numbers[0], numbers[1]));
     }
 
     private static int solution(int start, int end) {
-        int[] visited = new int[100001];
-        int[] queue = new int[100001];
-        int front = 0;
-        int rear = 0;
-        queue[rear++] = start;
+        int[] visited = new int[MAX + 1];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
         visited[start] = 1;
-        while (front < rear) {
-            int current = queue[front++];
-            if (current == end) return visited[current] - 1;
-            if (current - 1 >= 0 && visited[current - 1] == 0) {
-                queue[rear++] = current - 1;
-                visited[current - 1] = visited[current] + 1;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            if (current == end) {
+                return visited[current] - 1;
             }
-            if (current + 1 <= 100000 && visited[current + 1] == 0) {
-                queue[rear++] = current + 1;
-                visited[current + 1] = visited[current] + 1;
-            }
-            if (current * 2 <= 100000 && visited[current * 2] == 0) {
-                queue[rear++] = current * 2;
-                visited[current * 2] = visited[current] + 1;
+
+            int[] nextPositions = {current - 1, current + 1, current * 2};
+
+            for (int next : nextPositions) {
+                if (next >= 0 && next <= MAX && visited[next] == 0) {
+                    queue.add(next);
+                    visited[next] = visited[current] + 1;
+                }
             }
         }
-        return -1;
+
+        throw new IllegalArgumentException();
     }
 }
