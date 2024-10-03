9da46f8bb53938918b1734cb7a0b1bc2f4442d1b
hyoguoo <hyoguoo@gmail.com>
2024-10-03 09:14:01 +0900

    resolve: 스위치 켜고 끄기

--- src/main/java/implementation/simulation/TurnOnOffSwitch.java
+++ src/main/java/implementation/simulation/TurnOnOffSwitch.java
@@ -18,21 +18,61 @@ public class TurnOnOffSwitch {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         bufferedReader.readLine();
-        int[] switches = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int n = Integer.parseInt(bufferedReader.readLine());
+        int[] switches = Arrays.stream(bufferedReader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-        Switch switcher = new Switch(switches);
+        int queryCount = Integer.parseInt(bufferedReader.readLine());
 
-        while (n-- > 0) {
-            int[] info = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            if (info[0] == 1) switcher.switchMultiple(info[1]);
-            else switcher.switchSymmetrically(info[1]);
+        Query[] queries = new Query[queryCount];
+        for (int i = 0; i < queryCount; i++) {
+            String[] query = bufferedReader.readLine().split(" ");
+            queries[i] = new Query(Integer.parseInt(query[0]), Integer.parseInt(query[1]));
         }
 
-        System.out.println(switcher);
+        System.out.print(solution(switches, queries));
+    }
+
+    public static String solution(int[] switches, Query[] queries) {
+        Switch switcher = new Switch(switches);
+
+        Arrays.stream(queries)
+                .forEach(switcher::executeQuery);
+
+        return switcher.toString();
+    }
+
+    enum SwitchType {
+        MULTIPLE(1),
+        SYMMETRIC(2);
+
+        private final int value;
+
+        SwitchType(int value) {
+            this.value = value;
+        }
+
+        public static SwitchType of(int value) {
+            return Arrays.stream(values())
+                    .filter(switchType -> switchType.value == value)
+                    .findFirst()
+                    .orElseThrow();
+        }
+    }
+
+    static class Query {
+
+        private final SwitchType type;
+        private final int index;
+
+        public Query(int type, int index) {
+            this.type = SwitchType.of(type);
+            this.index = index;
+        }
     }
 
     static class Switch {
+
         boolean[] switches;
 
         public Switch(int[] switches) {
@@ -42,19 +82,34 @@ public class TurnOnOffSwitch {
             }
         }
 
-        public void switchMultiple(int number) {
+        public void executeQuery(Query query) {
+            switch (query.type) {
+                case MULTIPLE:
+                    switchMultiple(query.index);
+                    break;
+                case SYMMETRIC:
+                    switchSymmetrically(query.index);
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
+
+        private void switchMultiple(int number) {
             for (int i = number; i < switches.length; i += number) {
                 switches[i] = !switches[i];
             }
         }
 
-        public void switchSymmetrically(int center) {
+        private void switchSymmetrically(int center) {
             int left = center - 1;
             int right = center + 1;
 
             switches[center] = !switches[center];
             while (left >= 1 && right < switches.length) {
-                if (switches[left] != switches[right]) break;
+                if (switches[left] != switches[right]) {
+                    break;
+                }
                 switches[left] = !switches[left];
                 switches[right] = !switches[right];
                 left--;
@@ -68,8 +123,12 @@ public class TurnOnOffSwitch {
             final int LINE_LENGTH = 20;
 
             for (int i = 1; i < switches.length; i++) {
-                stringBuilder.append(switches[i] ? 1 : 0).append(" ");
-                if (i % LINE_LENGTH == 0) stringBuilder.append("\n");
+                stringBuilder
+                        .append(switches[i] ? 1 : 0)
+                        .append(" ");
+                if (i % LINE_LENGTH == 0) {
+                    stringBuilder.append("\n");
+                }
             }
 
             return stringBuilder.toString().trim();
