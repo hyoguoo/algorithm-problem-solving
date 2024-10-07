16f3eafa1110270d08db3574117a822503beb4a1
hyoguoo <hyoguoo@gmail.com>
2024-10-07 15:04:51 +0900

    resolve: 택배 배송

--- src/main/java/graph/dijkstra/PackageDelivery.java
+++ src/main/java/graph/dijkstra/PackageDelivery.java
@@ -13,43 +13,62 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class PackageDelivery {
 
-    static int VERTEX_COUNT, EDGE_COUNT;
-    static List<Edge>[] EDGE_LIST;
+    private static final int START_VERTEX = 1;
 
     public static void main(String[] args) throws IOException {
-        init();
-        solution();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[] info = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Map<Integer, List<Edge>> graph = new HashMap<>();
+        int vertexCount = info[0];
+        int edgeCount = info[1];
+        for (int i = 0; i < edgeCount; i++) {
+            int[] edgeInfo = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int from = edgeInfo[0];
+            int to = edgeInfo[1];
+            int cost = edgeInfo[2];
+            graph.computeIfAbsent(from, k -> new ArrayList<>()).add(new Edge(to, cost));
+            graph.computeIfAbsent(to, k -> new ArrayList<>()).add(new Edge(from, cost));
+        }
+
+        System.out.print(solution(graph, vertexCount));
     }
 
-    private static void solution() {
-        int[] dijkstra = dijkstra(1);
-        System.out.println(dijkstra[VERTEX_COUNT]);
+    private static int solution(Map<Integer, List<Edge>> graph, int vertexCount) {
+        int[] dijkstra = dijkstra(graph, vertexCount);
+        return dijkstra[vertexCount];
     }
 
-    private static int[] dijkstra(int start) {
-        int[] distance = new int[VERTEX_COUNT + 1];
-        for (int i = 0; i <= VERTEX_COUNT; i++) distance[i] = Integer.MAX_VALUE;
-        distance[start] = 0;
+    private static int[] dijkstra(Map<Integer, List<Edge>> graph, int vertexCount) {
+        int[] distance = new int[vertexCount + 1];
+        for (int i = 0; i <= vertexCount; i++) {
+            distance[i] = Integer.MAX_VALUE;
+        }
+        distance[START_VERTEX] = 0;
 
-        boolean[] visited = new boolean[VERTEX_COUNT + 1];
+        boolean[] visited = new boolean[vertexCount + 1];
 
-        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>();
-        priorityQueue.add(new Edge(start, 0));
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
+        priorityQueue.add(new Edge(START_VERTEX, 0));
 
         while (!priorityQueue.isEmpty()) {
             Edge poll = priorityQueue.poll();
             int current = poll.to;
             int currentCost = poll.cost;
 
-            if (visited[current]) continue;
+            if (visited[current]) {
+                continue;
+            }
             visited[current] = true;
 
-            for (Edge edge : EDGE_LIST[current]) {
+            for (Edge edge : graph.getOrDefault(current, new ArrayList<>())) {
                 int next = edge.to;
                 int nextCost = edge.cost;
                 int newDistance = currentCost + nextCost;
@@ -59,38 +78,18 @@ public class PackageDelivery {
                 }
             }
         }
+
         return distance;
     }
 
-    private static void init() throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int[] info = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        VERTEX_COUNT = info[0];
-        EDGE_COUNT = info[1];
-        EDGE_LIST = new ArrayList[VERTEX_COUNT + 1];
-        for (int i = 0; i <= VERTEX_COUNT; i++) EDGE_LIST[i] = new ArrayList<>();
-        for (int i = 0; i < EDGE_COUNT; i++) {
-            int[] edgeInfo = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int from = edgeInfo[0];
-            int to = edgeInfo[1];
-            int cost = edgeInfo[2];
-            EDGE_LIST[from].add(new Edge(to, cost));
-            EDGE_LIST[to].add(new Edge(from, cost));
-        }
-    }
+    static class Edge {
 
-    static class Edge implements Comparable<Edge> {
-        int to;
-        int cost;
+        private final int to;
+        private final int cost;
 
         public Edge(int to, int cost) {
             this.to = to;
             this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Edge o) {
-            return this.cost - o.cost;
         }
     }
 }
