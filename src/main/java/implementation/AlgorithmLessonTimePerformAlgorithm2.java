0607549e113de4ec8a967378f762cb0b04a61760
hyoguoo <hyoguoo@gmail.com>
2024-12-31 17:34:23 +0900

    solve: 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ src/main/java/implementation/AlgorithmLessonTimePerformAlgorithm2.java
@@ -0,0 +1,25 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 24263
+ * Cheat Level: 0
+ * Algorithm: Implementation
+ */
+
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AlgorithmLessonTimePerformAlgorithm2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(solution(Integer.parseInt(bufferedReader.readLine())));
+    }
+
+    private static String solution(int n) {
+        return n + "\n" + 1;
+    }
+}
