80dd92c0b7bdbb509c55b7276facde73eea26eb6
hyoguoo <hyoguoo@gmail.com>
2025-11-06 17:15:19 +0900

    solve: UCPC에서 가장 쉬운 문제 번호는?

---
+++ src/main/java/implementation/EasiestProblemNumberInUCPC.java
@@ -0,0 +1,25 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 25311
+ * Cheat Level: 0
+ * Algorithm: Implementation
+ */
+
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EasiestProblemNumberInUCPC {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(solution(Integer.parseInt(bufferedReader.readLine())));
+    }
+
+    private static String solution(int year) {
+        return "A";
+    }
+}
