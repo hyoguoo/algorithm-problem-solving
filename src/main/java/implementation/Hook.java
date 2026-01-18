444f8d328c34c7b774ea111144de2a45bd95d560
Hyoseung Kwon <hyoguoo@gmail.com>
2026-01-18 18:24:09 +0900

    solve: Hook

---
+++ src/main/java/implementation/Hook.java
@@ -0,0 +1,21 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 10189
+ * Cheat Level: 0
+ * Algorithm: Implementation
+ */
+
+package implementation;
+
+public class Hook {
+
+    public static void main(String[] args) {
+        System.out.println(
+            "#  # #### #### #  #\n" +
+            "#### #  # #  # # #\n" +
+            "#### #  # #  # # #\n" +
+            "#  # #### #### #  #"
+        );
+    }
+}
