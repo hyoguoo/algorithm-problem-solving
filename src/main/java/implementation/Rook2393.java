e23fd67a10e95399d8d0b9f0da64fda9640b3e5c
Hyoseung Kwon <hyoguoo@gmail.com>
2026-02-01 16:56:54 +0900

    solve: Rook

---
+++ src/main/java/implementation/Rook2393.java
@@ -0,0 +1,30 @@
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 2393
+ * Cheat Level: 0
+ * Algorithm: Implementation
+ */
+
+package implementation;
+
+public class Rook {
+
+    public static void main(String[] args) {
+        System.out.print(
+            "  ___  ___  ___\n" +
+            "  | |__| |__| |\n" +
+            "  |           |\n" +
+            "   \\_________/\n" +
+            "    \\_______/\n" +
+            "     |     |\n" +
+            "     |     |\n" +
+            "     |     |\n" +
+            "     |     |\n" +
+            "     |_____|\n" +
+            "  __/       \\__\n" +
+            " /             \\\n" +
+            "/_______________\\"
+        );
+    }
+}
